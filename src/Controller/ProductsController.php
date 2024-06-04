af5abf9364cf453c5c1de03913632529c89ae61f
Umut <develop@kariyerfora.com>
2024-06-04 08:46:16 +0300

    test1

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Pimcore\Controller\FrontendController;
 use Pimcore\Model\DataObject\Product\Listing;
 use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\DataObject\ProductClass;
+use Pimcore\Model\DataObject\ProductClass\Listing as ProductClassListing;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,18 +18,17 @@ class ProductsController extends FrontendController
      */
     public function listAction(Request $request): Response
     {
+        // Create a new Listing object
         $products = new Listing();
-        $products->setOrderKey('name'); 
-        $products->setOrder('asc'); 
+        $products->setOrderKey('name'); // Example: Order by name
+        $products->setOrder('asc'); // Example: Order ascending
 
+        // Fetch the products
         $productList = $products->load();
 
-        $productClasses = [];
-        foreach ($productList as $product) {
-            if (!in_array($product->getProductClass(), $productClasses)) {
-                $productClasses[] = $product->getProductClass();
-            }
-        }
+        // Fetch product classes
+        $productClassListing = new ProductClassListing();
+        $productClasses = $productClassListing->load();
 
         return $this->render('products/list.html.twig', [
             'products' => $productList,
