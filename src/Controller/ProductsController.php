ff30a93dfbbccdbec9b8b0beeb6fc97d8ec4916e
Umut <develop@kariyerfora.com>
2024-06-06 17:36:45 +0300

    a

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -162,16 +162,19 @@ class ProductsController extends FrontendController
 
         // if a new color, first add its variation to parent
         if (!empty($newColor) && !empty($allVariants['colors'][0])) {
+            error_log('!empty($newColor) && !empty($allVariants[\'colors\'][0])');
             $this->addVariant($product, $newColor, '', $newColor, false);
         }
 
         $colorVariants = $this->colorVariantObjects($product);
 
         if (!empty($newColor) && empty($allVariants['colors'][0])) {
+            error_log('!empty($newColor) && empty($allVariants[\'colors\'][0])');
             $colorVariants[$newColor] = $this->updateVariant($colorVariants[$allVariants['colors'][0]], $product, $newColor, '', $newColor, false);
         }
 
         if (!empty($newSize) && !empty($allVariants['sizes'][0])) {
+            error_log('!empty($newSize) && !empty($allVariants[\'sizes\'][0])');
             foreach ($colorVariants as $color => $variant) {
                 $this->addVariant($variant, $newSize.' '.$color, $newSize, '', true);
             }
