4053d1dd07272335cd21c48dabb8c15feb81ff22
DevelopKariyer <develop@kariyerfora.com>
2024-09-05 14:40:10 +0300

    a

--- src/Utils/AmazonConnector.php
+++ src/Utils/AmazonConnector.php
@@ -63,7 +63,7 @@ class AmazonConnector implements MarketplaceConnectorInterface
         }
         print_r($countryCodes);
         $fbaInventory = $this->amazonSellerConnector->fbaInventoryV1();
-        $response = $fbaInventory->getInventorySummaries(['granularityType' => 'Marketplace']);
+        $response = $fbaInventory->getInventorySummaries('Marketplace');
         print_r($response->json());
         exit;
     }
