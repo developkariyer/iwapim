00187a550c6eb44b1a700318c842d8c50a60677b
DevelopKariyer <develop@kariyerfora.com>
2024-09-07 22:31:48 +0300

    a

--- src/Utils/AmazonConnector.php
+++ src/Utils/AmazonConnector.php
@@ -379,7 +379,7 @@ class AmazonConnector implements MarketplaceConnectorInterface
     public function catalogItems()
     {
         $catalogConnector = $this->amazonSellerConnector->catalogItemsV20220401();
-        $response = $catalogConnector->getCatalogItems(
+        $response = $catalogConnector->getCatalogItem(
             marketplaceIds: [AmazonMerchantIdList::$amazonMerchantIdList[$this->countryCodes[0]]],
             sellerId: $this->marketplace->getMerchantId(),
         );
