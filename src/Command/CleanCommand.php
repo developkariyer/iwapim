fb44e2d5fbec5dfcc83f675d27fee2c6ff8e5da3
DevelopKariyer <develop@kariyerfora.com>
2024-09-10 20:43:36 +0300

    a

--- src/Command/CleanCommand.php
+++ src/Command/CleanCommand.php
@@ -118,9 +118,9 @@ class CleanCommand extends AbstractCommand
                             if ($mainProduct = reset($mainProductArray)) {
                                 $mainProduct->addVariant($newVariantProduct);
                                 echo "{$child->getId()} => {$newVariantProduct->getId()} ";
+                                $child->delete();
+                                echo "{$child->getId()} deleted\n";                
                             }
-                            $child->delete();
-                            echo "{$child->getId()} deleted\n";                
                         }
                     }
                 }
