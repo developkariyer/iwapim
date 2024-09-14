186d87cbc46b7e4c0102ebf1d991bc4d07d9f9e8
DevelopKariyer <develop@kariyerfora.com>
2024-09-14 17:58:23 +0300

    a

--- src/Model/DataObject/CostModel.php
+++ src/Model/DataObject/CostModel.php
@@ -3,16 +3,37 @@
 namespace App\Model\DataObject;
 
 use Pimcore\Model\DataObject\Concrete;
-use Pimcore\Model\DataObject\Currency\Listing;
 
 class CostModel extends Concrete
 {
-    public function getCost($product)
+    public function getCost($product): string
     {
         $totalCost = '0.00';
         foreach ($this->getCostNodes() as $costNode) {
-            error_log($costNode->getFactor());
+            $totalCost = bcAdd(
+                $totalCost,
+                match ($costNode->getFactor()) {
+                    'Beher Ürün' => bcmul(
+                        $costNode->getUnitPrice(), 
+                        $costNode->getSarf()
+                    ),
+                    'Ebat m2' => bcmul(
+                        $costNode->getUnitPrice(), 
+                        bcmul($costNode->getSarf(), number_format($product->getArea(), 2, '.', ''))
+                    ),
+                    'Ambalaj m2' => bcmul(
+                        $costNode->getUnitPrice(),
+                        bcmul($costNode->getSarf(), number_format($product->getPackageArea(), 2, '.', ''))
+                    ),
+                    'Kesim Detay' => bcmul(
+                        $costNode->getUnitPrice(),
+                        bcmul($costNode->getSarf(), number_format($product->getCutComplexity(), 2, '.', ''))
+                    ),
+                    default => '0.00',
+                }
+            );
         }
+        return $totalCost;
     }
-    
+
 }
