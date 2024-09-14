6878237b458c1ffa227f20a5244e77cc923ce31b
DevelopKariyer <develop@kariyerfora.com>
2024-09-14 12:16:20 +0300

    a

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -23,11 +23,49 @@ class ReportController extends FrontendController
         $group = GroupProduct::getById($groupId);
         $products = $group->getProducts();
         $pricingModels = $group->getPricingModels();
+        $productTwig = [];
+        /*
+                        <td>{{ product.iwasku }}</td>
+                <td>{{ product.productCategory }}</td>
+                <td>{{ product.productIdentifier }}</td>
+                <td>{{ product.name }}</td>
+                <td>{{ product.variationSize }}</td>
+                <td>{{ product.variationColor }}</td>
+                <td>{{ product.packageDimension1 }}</td>
+                <td>{{ product.packageDimension2 }}</td>
+                <td>{{ product.packageDimension3 }}</td>
+                <td>{{ product.packageWeight }}</td>
+                <td><img src="{{ product.imageUrl }}" alt="Product Image" style="max-width: 100px; max-height: 100px;"></td>
+                <td>{{ product.productCost }}</td>
+                <td>{{ product.productDimension1 }}</td>
+                <td>{{ product.productDimension2 }}</td>
+                <td>{{ product.productDimension3 }}</td>
+
+        */
+        foreach ($products as $product) {
+            $productTwig[] = [
+                'iwasku' => $product->getIwasku(),
+                'productCategory' => $product->getProductCategory(),
+                'productIdentifier' => $product->getProductIdentifier(),
+                'name' => $product->getName(),
+                'variationSize' => $product->getVariationSize(),
+                'variationColor' => $product->getVariationColor(),
+                'packageDimension1' => $product->getPackageDimension1(),
+                'packageDimension2' => $product->getPackageDimension2(),
+                'packageDimension3' => $product->getPackageDimension3(),
+                'packageWeight' => $product->getPackageWeight(),
+                'imageUrl' => $product->getImage()->getThumbnail()->getUri(),
+                'productCost' => $product->getProductCost(),
+                'productDimension1' => $product->getProductDimension1(),
+                'productDimension2' => $product->getProductDimension2(),
+                'productDimension3' => $product->getProductDimension3(),
+            ];
+        }
 
         return $this->render(
             '202409/group.html.twig', 
             [
-                'products' => $products,
+                'products' => $productTwig,
             ]
         );
     }
