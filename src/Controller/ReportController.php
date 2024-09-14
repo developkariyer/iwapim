842e7d429d899a67d2a2780e1a22c1f714619ad8
DevelopKariyer <develop@kariyerfora.com>
2024-09-14 12:06:14 +0300

    a

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -6,12 +6,6 @@ use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Pimcore\Model\DataObject\Product\Listing as ProductListing;
-use Pimcore\Model\DataObject\ShopifyListing\Listing as ShopifyListingListing;
-use Pimcore\Model\DataObject\ShopifyVariant\Listing as ShopifyVariantListing;
-use Pimcore\Model\DataObject\AmazonVariant\Listing as AmazonVariantListing;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use GuzzleHttp\Client;
 
 
 class ReportController extends FrontendController
