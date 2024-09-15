06a28b90c0e7c24189f4e0d397427fc54bd9a5c9
DevelopKariyer <develop@kariyerfora.com>
2024-09-15 09:47:24 +0300

    a

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -95,6 +95,76 @@ class ReportController extends FrontendController
     }
 
     /**
+     * @Route("/report/connected", name="report_connected")
+     */
+    public function connectedAction(Request $request): Response
+    {
+        Product::setGetInheritedValues(true);
+        $products = [];
+        $productListing = new Product\Listing();
+        foreach ($productListing as $product) {
+            if (!$product->level() || !$product->getListingItems()) {
+                continue;
+            }
+            $products[] = $product;
+        }
+        $priceTemplate = Marketplace::getMarketplaceListAsArrayKeys();
+        $pricingModels = [];
+        $productTwig = [];
+        $modelTwig = [];
+        foreach ($products as $product) {
+            if (!($imageUrl = $product->getInheritedField('imageUrl'))) {
+                $imageUrl = ($image = $product->getInheritedField('image')) ? $image->getFullPath() : '';
+            }
+            $productModels = [];
+            foreach ($pricingModels as $pricingModel) {
+                $modelKey = $pricingModel->getKey();
+                $productModels[$modelKey] = 123;
+            }
+            $prices = $priceTemplate;
+            foreach ($product->getListingItems() as $listingItem) {
+                if ($listingItem->getMarketplace()->getMarketplaceType() === 'Amazon') {
+                    continue;
+                } else {
+                    $urlLink = $listingItem->getUrlLink();
+                    $urlLink = $urlLink instanceof Link ? $urlLink->getHref() : '';
+                    $prices[$listingItem->getMarketplace()->getKey()] = [
+                        'priceTL' => number_format(Currency::convertCurrency($listingItem->getSaleCurrency() ?? 'US DOLLAR', $listingItem->getSalePrice()), 2, '.', ','),
+                        'priceUS' => number_format(Currency::convertCurrency($listingItem->getSaleCurrency() ?? 'US DOLLAR', $listingItem->getSalePrice(), 'US DOLLAR'), 2, '.', ','),
+                        'urlLink' => $urlLink,
+                    ];
+                }
+            }
+            $productTwig[] = [
+                'iwasku' => $product->getIwasku(),
+                'productCategory' => $product->getInheritedField('productCategory'),
+                'productIdentifier' => $product->getInheritedField('productIdentifier'),
+                'name' => $product->getInheritedField('name'),
+                'variationSize' => $product->getVariationSize(),
+                'variationColor' => $product->getVariationColor(),
+                'productDimension1' => $product->getInheritedField('productDimension1'),
+                'productDimension2' => $product->getInheritedField('productDimension2'),
+                'productDimension3' => $product->getInheritedField('productDimension3'),
+                'packageWeight' => $product->getInheritedField('packageWeight'),
+                'imageUrl' => $imageUrl,
+                'productCost' => $product->getProductCost(),
+                'models' => $productModels,
+                'bundleItems' => $product->getBundleItems(),
+                'prices' => $prices,
+            ];
+        }
+        return $this->render(
+            '202409/group.html.twig', 
+            [
+                'title' => 'Bağlanmış Ürünler',
+                'products' => $productTwig,
+                'models' => $modelTwig,
+                'markets' => array_keys($priceTemplate),
+            ]
+        );
+    }
+
+    /**
      * @Route("/report/cost/{product_id}", name="report_cost")
      */
     public function costAction(Request $request): Response
