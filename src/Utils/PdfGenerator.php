188382f667ccd4a4ea88e85a484632137a9575bc
DevelopKariyer <develop@kariyerfora.com>
2024-09-25 11:12:38 +0300

    a

--- src/Utils/PdfGenerator.php
+++ src/Utils/PdfGenerator.php
@@ -124,7 +124,7 @@ class PdfGenerator
     
         // Use a font that supports Turkish characters
         //$pdf->AddFont('DejaVu','','DejaVuSansCondensed',true);
-        $pdf->SetFont('DejaVuSansCondensed', '', 8);
+        $pdf->SetFont('dejavu', '', 8);
     
         // Add QR code (optional)
         //$pdf->Image($qrcode, 2, 2, 10, 10); // Adjust the position and size as needed
