e583b4ae25379bfb553fddf0a5303b74e06d3896
DevelopKariyer <develop@kariyerfora.com>
2024-09-25 16:30:25 +0300

    a

--- src/Utils/PdfGenerator.php
+++ src/Utils/PdfGenerator.php
@@ -138,8 +138,8 @@ class PdfGenerator
         $pdf->SetXY(1, 18);
         $pdf->MultiCell(30, 2, mb_convert_encoding("PN: {$product->getInheritedField("iwasku")}\nSN: {$product->getInheritedField("productIdentifier")}", 'windows-1254', 'UTF-8'), 0, 'L');
     
-        $text =  $product->getInheritedField("nameEnglish") . " (";
-        $text .= $product->getInheritedField("variationSize"). "-" . $product->getInheritedField("variationColor") . ")";
+        $text =  $product->getInheritedField("productIdentifier") ." ";
+        $text .= $product->getInheritedField("variationSize"). " " . $product->getInheritedField("variationColor") ;
 
         $pdf->SetXY(1, 23);
         $pdf->MultiCell(56, 2, mb_convert_encoding(Utility::keepSafeChars(Utility::removeTRChars($text)), 'windows-1254', 'UTF-8'), 0, 'L');
