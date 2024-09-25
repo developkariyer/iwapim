1b88655287268aa68ee19404eabd7670f92993e6
enes <burdaenesvar@gmail.com>
2024-09-25 09:14:18 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -54,7 +54,7 @@ class WisersellCommand extends AbstractCommand
 
     protected function getAccessToken()
     {
-        $token_file = "../tmp/wisersell_access_token.json";
+        $token_file = "/var/www/iwapim/tmp/wisersell_access_token.json";
         if (file_exists($token_file)) {
             echo "Token file exists.";
             $token = json_decode(file_get_contents($token_file), true);
