b1fa087f3b1bc1be9315f397d41d5e596858f6af
enes <burdaenesvar@gmail.com>
2024-09-25 10:00:40 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -92,18 +92,19 @@ class WisersellCommand extends AbstractCommand
             echo "cURL Error: $error";
         } else {
             $result = json_decode($response, true);
-            if (isset($result['taken'])) {
-                echo "Bearer Token: " . $result['taken'];
-                $token_file = "/var/www/iwapim/tmp/wisersell_access_token.json";
-                if (file_exists($token_file)) {
-                    unlink($token_file); 
-                    echo "Old token file deleted.\n";
-                }
-                file_put_contents($token_file, json_encode(['token' => $result['taken']], JSON_PRETTY_PRINT));
-                echo "New token saved to file.\n";
-            } else {
-                echo "Failed to get bearer token. Response: " . $response;
-            }
+            echo "Result: " . $result;
+            // if (isset($result['taken'])) {
+            //     echo "Bearer Token: " . $result['taken'];
+            //     $token_file = "/var/www/iwapim/tmp/wisersell_access_token.json";
+            //     if (file_exists($token_file)) {
+            //         unlink($token_file); 
+            //         echo "Old token file deleted.\n";
+            //     }
+            //     file_put_contents($token_file, json_encode(['token' => $result['taken']], JSON_PRETTY_PRINT));
+            //     echo "New token saved to file.\n";
+            // } else {
+            //     echo "Failed to get bearer token. Response: " . $response;
+            // }
         }
         curl_close($ch);
     }
