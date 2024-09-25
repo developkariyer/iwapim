6ff62f38efd3e2cd0e8c744b49fc1ee203d0c50b
enes <burdaenesvar@gmail.com>
2024-09-25 12:57:36 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -171,7 +171,7 @@ class WisersellCommand extends AbstractCommand{
     protected function addCategory($token,$category){
         $url = "https://dev2.wisersell.com/restapi/category"; 
         $data = [
-            "name" => "Cam"
+            "name" => $category
         ];
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
