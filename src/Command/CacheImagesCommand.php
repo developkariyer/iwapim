80bb6a36fef80415fc49bca97592ff4f4e37b27b
DevelopKariyer <develop@kariyerfora.com>
2024-09-28 23:11:50 +0300

    a

--- src/Command/CacheImagesCommand.php
+++ src/Command/CacheImagesCommand.php
@@ -52,7 +52,7 @@ class CacheImagesCommand extends AbstractCommand
         $listingObject = new VariantProduct\Listing();
         $listingObject->setUnpublished(true);
         $pageSize = 150;
-        $offset = 0;
+        $offset = 20000;
 
         while (true) {
             $listingObject->setLimit($pageSize);
