c9179ce1bd767cc57a96483135fd57dd9016968e
DevelopKariyer <develop@kariyerfora.com>
2024-10-02 14:52:51 +0300

    a

--- src/MarketplaceConnector/ShopifyConnector.php
+++ src/MarketplaceConnector/ShopifyConnector.php
@@ -3,6 +3,7 @@
 namespace App\MarketplaceConnector;
 
 use Pimcore\Model\DataObject\VariantProduct;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpClient\ScopingHttpClient;
 
 use App\Utils\Utility;
