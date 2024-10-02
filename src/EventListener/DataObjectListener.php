c6121b6a4f6f56127e84caab897f99a12381ae09
DevelopKariyer <develop@kariyerfora.com>
2024-10-02 11:34:05 +0300

    a

--- src/EventListener/DataObjectListener.php
+++ src/EventListener/DataObjectListener.php
@@ -164,7 +164,7 @@ class DataObjectListener implements EventSubscriberInterface
                 }
             }
         }
-        $dependencyObject = Dependency::getBySourceId($object->getId());
+        $dependencyObject = Dependency::getBySourceId($object->getId(), 'object');
         $dependencies = $dependencyObject->getRequiredBy();
         foreach ($dependencies as $dependency) {
             error_log(get_class($dependency));
