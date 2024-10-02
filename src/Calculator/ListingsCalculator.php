7c24607bd6950e72c0457bd1b647719cb119db02
DevelopKariyer <develop@kariyerfora.com>
2024-10-02 11:55:08 +0300

    a

---
+++ src/Calculator/ListingsCalculator.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace App\Calculator;
+
+use Pimcore\Model\DataObject\Concrete;
+use Pimcore\Model\DataObject\ClassDefinition\CalculatorClassInterface;
+use Pimcore\Model\DataObject\Data\CalculatedValue;
+use Pimcore\Model\DataObject\Product;
+
+
+class ListingsCalculator implements CalculatorClassInterface
+{
+    public function compute(Concrete $object, CalculatedValue $context): string
+    {
+        if ($object instanceof Product) {
+            return count($object->getListingItems());
+        }
+        return "";
+    }
+
+    public function getCalculatedValueForEditMode(Concrete $object, CalculatedValue $context): string
+    {
+        return $this->compute($object, $context);
+    }
+
+}
