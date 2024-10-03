02ef0dd34db0b293db29eb789b73b666370f4915
DevelopKariyer <develop@kariyerfora.com>
2024-10-03 22:28:54 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -144,7 +144,7 @@ class WisersellCommand extends AbstractCommand
 
         $listingObject = new Product\Listing();
         $listingObject->setUnpublished(false);
-        $listingObject->setCondition("iwasku IS NOT NULL AND iwasku != '' AND (wisersellId IS NULL OR wisersellId = '')");
+        $listingObject->setCondition("iwasku IS NOT NULL AND iwasku != ''");
         $pageSize = 50;
         $offset = 0;
         $productBucket = [];
