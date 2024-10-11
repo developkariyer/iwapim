08237c38dcd758423de5729124d48007760af751
enes <burdaenesvar@gmail.com>
2024-10-11 11:15:52 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -99,10 +99,7 @@ class WisersellCommand extends AbstractCommand
             $this->syncStores();
         }
         foreach ($this->storeList as $marketplace) {
-            $variantProducts = $marketplace->getRelated('marketplace');
-            foreach ($variantProducts as $variantProduct) {
-                print_r($variantProduct);
-            }
+            print_r($marketplace);
         }
     }
 
