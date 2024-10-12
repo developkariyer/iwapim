81a32d385c490d4dd99e505bbfd88bf753864609
enes <burdaenesvar@gmail.com>
2024-10-12 13:39:25 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -174,7 +174,8 @@ class WisersellCommand extends AbstractCommand
                 ];
                 $jsonListingData = json_encode($listingData);
                 print_r($jsonListingData);
-                $response = $this->request(self::$apiUrl['listing'], 'POST','', $jsonListingData);
+                //$response = $this->request(self::$apiUrl['listing'], 'POST','', $jsonListingData);
+                $response = $this->httpClient->request("POST", self::$apiUrl['listing'], ['body' => $listingData]);
                 print_r($response->getContent());
                 $listingData = [];
                 // $code = $variantProduct->getWisersellVariantCode();
