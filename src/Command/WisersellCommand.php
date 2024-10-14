13b71023a1d49ab1db12c2d339bba9f7f29b5c45
enes <burdaenesvar@gmail.com>
2024-10-14 14:14:22 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -118,12 +118,8 @@ class WisersellCommand extends AbstractCommand
     protected function searchAndUpdateVariantProducts($responseArray)
     {
         foreach ($responseArray['rows'] as $row) {
-            //$variantProduct = VariantProduct::findByField('wisersellVariantCode', $row['code']);
-            //$variantProduct = VariantProduct::findOneByField('uniqueMarketplaceId', $row['storeproductid']);
-            $variantProducts = new \Pimcore\Model\DataObject\VariantProduct\Listing();
-            $variantProducts->setCondition("JSON_EXTRACT(apiResponseJson, '$.product_id') = ?", [$row['storeproductid']]);
-            $variantProducts->setLimit(1);
-            $variantProduct = $variantProducts->load();
+            $variantProduct = VariantProduct::findOneByField('uniqueMarketplaceId', $row['storeproductid']);
+            
             echo "\nProcessing {$row['code']}... \n";
             if ($variantProduct instanceof VariantProduct) {
                 echo "\nFound in PIM... \n";
