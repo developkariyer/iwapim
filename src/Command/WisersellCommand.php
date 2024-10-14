cca7c78240ade9ba29aa5dcc20668605a557ea25
enes <burdaenesvar@gmail.com>
2024-10-14 10:14:06 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -150,7 +150,7 @@ class WisersellCommand extends AbstractCommand
                 $pageSize = 100;
                 $page = 0;
                 $searchData = [  
-                    "shopId" => $store['id'],
+                    "shopId" => $store['shopId'],
                     "page" => $page,
                     "pageSize" => $pageSize
                 ];
