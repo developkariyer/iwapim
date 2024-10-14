1b01468f85f68b4ceda0d16d17d438888d245855
enes <burdaenesvar@gmail.com>
2024-10-14 21:34:16 +0300

    a

--- src/Calculator/WisersellCodeCalculator.php
+++ src/Calculator/WisersellCodeCalculator.php
@@ -18,35 +18,35 @@ class WisersellCodeCalculator implements CalculatorClassInterface
 
     private function calculateWisersellCode(Concrete $object): string
     {
-        $marketplaceObject = $object->getMarketplace();
-        $marketplaceType = $marketplaceObject->getMarketplaceType();
-        $storeProductId = match ($marketplaceType) {
-            'Etsy' => json_decode($object->jsonRead('apiResponseJson'), true)["product_id"],
-            'Amazon' =>  json_decode($object->jsonRead('apiResponseJson'), true)["asin"],
-            'Shopify' => json_decode($object->jsonRead('apiResponseJson'), true)["product_id"],  
-            'Trendyol' => json_decode($object->jsonRead('apiResponseJson'), true)["productCode"],
-        };
-        $variantCode = match ($marketplaceType) {
-            'Etsy' => json_decode($variantProduct->jsonRead('parentResponseJson'), true) ["listing_id"],
-            'Shopify' => json_decode($variantProduct->jsonRead('apiResponseJson'), true)["id"],  
-            'Trendyol' => json_decode($variantProduct->jsonRead('apiResponseJson'), true)["platformListingId"],
-        };
-        $storeId = match ($marketplaceType) {
-            'Etsy' => $marketplace->getShopId(),
-            'Amazon' => $marketplace->getMerchantId(),
-            //'Shopify' => $marketplace->getShopifyStoreId(),  
-            'Trendyol' => $marketplace->getTrendyolSellerId(),
-        };
-        $data = "";
-        if($marketplaceType !== 'Amazon') {
-            $data = "{$storeId}_{$storeProductId}_{$variantCode}";
-        }
-        else {
-            $data = "{$storeId}_{$storeProductId}";
-        }
-        $hash = hash('sha1', $data);
-        return number_format($hash);
-        //return "WisersellCode";
+        // $marketplaceObject = $object->getMarketplace();
+        // $marketplaceType = $marketplaceObject->getMarketplaceType();
+        // $storeProductId = match ($marketplaceType) {
+        //     'Etsy' => json_decode($object->jsonRead('apiResponseJson'), true)["product_id"],
+        //     'Amazon' =>  json_decode($object->jsonRead('apiResponseJson'), true)["asin"],
+        //     'Shopify' => json_decode($object->jsonRead('apiResponseJson'), true)["product_id"],  
+        //     'Trendyol' => json_decode($object->jsonRead('apiResponseJson'), true)["productCode"],
+        // };
+        // $variantCode = match ($marketplaceType) {
+        //     'Etsy' => json_decode($variantProduct->jsonRead('parentResponseJson'), true) ["listing_id"],
+        //     'Shopify' => json_decode($variantProduct->jsonRead('apiResponseJson'), true)["id"],  
+        //     'Trendyol' => json_decode($variantProduct->jsonRead('apiResponseJson'), true)["platformListingId"],
+        // };
+        // $storeId = match ($marketplaceType) {
+        //     'Etsy' => $marketplace->getShopId(),
+        //     'Amazon' => $marketplace->getMerchantId(),
+        //     //'Shopify' => $marketplace->getShopifyStoreId(),  
+        //     'Trendyol' => $marketplace->getTrendyolSellerId(),
+        // };
+        // $data = "";
+        // if($marketplaceType !== 'Amazon') {
+        //     $data = "{$storeId}_{$storeProductId}_{$variantCode}";
+        // }
+        // else {
+        //     $data = "{$storeId}_{$storeProductId}";
+        // }
+        // $hash = hash('sha1', $data);
+        // return number_format($hash);
+        return "WisersellCode";
     }
 
     public function getCalculatedValueForEditMode(Concrete $object, CalculatedValue $context): string
