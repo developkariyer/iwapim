41d268d9523f79a155a20ba2fdbab89bdf98a2d8
enes <burdaenesvar@gmail.com>
2024-10-15 10:50:59 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -315,7 +315,7 @@ class WisersellCommand extends AbstractCommand
                 }
                 $listingData = [];*/
                 $count++;
-                if ($count ==5) {
+                if ($count ==7) {
                     break;
                 }
             }
