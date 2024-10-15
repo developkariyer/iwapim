c532e9618522d7d1c6ccd95742b216d5f49ef87d
enes <burdaenesvar@gmail.com>
2024-10-15 11:00:19 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -293,7 +293,7 @@ class WisersellCommand extends AbstractCommand
                         "variantStr" => (string)$variantCode
                 ];
                 $listingBucket[] = $listingData;
-                if (count($listingBucket) >= 100) {
+                if (count($listingBucket) <= 100) {
                     $this->addListingBucketToWisersell($listingBucket,$id);
                     $listingBucket = [];
                 }
