68c5cc12a373c2e653a4bf1f959c0133bfb022a7
enes <burdaenesvar@gmail.com>
2024-10-15 11:51:26 +0300

    a

--- src/Command/WisersellCommand.php
+++ src/Command/WisersellCommand.php
@@ -97,14 +97,14 @@ class WisersellCommand extends AbstractCommand
         if($input->getOption('calculatecode')) {
             $this->calculateWisersellCode();
         }
-        $codesToDelete = [
+        /*$codesToDelete = [
             'be7170082b30bd3798ff1b90d88bd78349e012bd',
             '2d1d9a4b082002c7a3fce7f62147cc8f4e8a4d01'
         ];
         foreach ($codesToDelete as $code) {
             $response = $this->request(self::$apiUrl['listing'], 'DELETE', "/{$code}", []);
             print_r($response->getContent());
-        }
+        }*/
       
         return Command::SUCCESS;
     }
