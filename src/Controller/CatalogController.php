7b2690ed47f9836066199b545d9db6959ad2f740
developkariyer <develop@kariyerfora.com>
2024-10-23 00:41:07 +0300

    a

---
+++ src/Controller/CatalogController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+use Pimcore\Controller\FrontendController;
+use Pimcore\Model\DataObject\GroupProduct;
+use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\DataObject\Currency;
+use Pimcore\Model\DataObject\Marketplace;
+use Pimcore\Model\DataObject\Data\Link;
+use Pimcore\Asset;
+
+class CatalogController extends FrontendController
+{
+    
+    /**
+     * @Route("/catalog", name="catalog")
+     */
+    public function catalogAction(): Response
+    {
+        $db = \Pimcore\Db::get();
+        $sql = "SELECT DISTINCT SUBSTRING_INDEX(productCode, '-', 1) AS productType FROM object_product ORDER BY productType";
+        $result = $db->fetchAllAssociative($sql);
+        $productTypes = array_column($result, 'productType');
+
+        return $this->render('catalog/catalog.html.twig', [
+            'productTypes' => $productTypes
+        ]);
+
+    }
+
+}
