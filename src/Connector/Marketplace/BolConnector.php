67ec7215e65de9b2830893a93f7e144d72cca8da
enes <burdaenesvar@gmail.com>
2024-10-22 10:37:37 +0300

    a

--- src/Connector/Marketplace/BolConnector.php
+++ src/Connector/Marketplace/BolConnector.php
@@ -251,7 +251,7 @@ class BolConnector extends MarketplaceConnectorAbstract
     public function downloadOrders()
     {
         $db = \Pimcore\Db::get();
-
+        $page = 0;
         do {
             $params = ['status' => 'ALL', 'page' => $page, 'fulfilment-method' => 'ALL'];
             $data = $this->downloadExtra(static::$apiUrl['orders'], 'GET', '',$params);
