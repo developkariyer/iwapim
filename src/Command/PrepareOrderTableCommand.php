2928d41b9044533db49fac37d0a33f5ee28dc6a3
enes <burdaenesvar@gmail.com>
2024-10-24 14:17:57 +0300

    a

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -617,18 +617,19 @@ class PrepareOrderTableCommand extends AbstractCommand
         $results = $db->fetchAllAssociative($sql); 
         foreach ($results as $row) {
             $referringSite = $row['referring_site'];
-            $parsedUrl = parse_url($referringSite);
-            if (isset($parsedUrl['host'])) {
-                $host = $parsedUrl['host'];
-                $domain = preg_replace('/^www\./', '', $host);
-                $updateQuery = "
-                    UPDATE iwa_marketplace_orders_line_items 
-                    SET referring_site_domain = $domain
-                    WHERE referring_site = $referringSite
-                ";
-                $stmt = $db->prepare($updateQuery);
-                $stmt->execute();
-               
+            if (filter_var($referringSite, FILTER_VALIDATE_URL)) {
+                $parsedUrl = parse_url($referringSite);
+                if (isset($parsedUrl['host'])) {
+                    $host = $parsedUrl['host'];
+                    $domain = preg_replace('/^www\./', '', $host);
+                    $updateQuery = "
+                        UPDATE iwa_marketplace_orders_line_items 
+                        SET referring_site_domain = $domain
+                        WHERE referring_site = $referringSite
+                    ";
+                    $stmt = $db->prepare($updateQuery);
+                    $stmt->execute();
+                }
             }
 
         }
