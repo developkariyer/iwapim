9e653617fbc3ea067198096b16ee8497535fe268
developkariyer <develop@kariyerfora.com>
2024-11-02 18:48:25 +0100

    a

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -84,7 +84,7 @@ class CatalogController extends FrontendController
             foreach ($children as $child) {
                 $variationSizeList[] = $child['variationSize'];
                 $variationColorList[] = $child['variationColor'];
-                $tooltip = htmlspecialchars("Size: {$child['variationSize']} | Color: {$child['variationColor']}");
+                $tooltip = htmlspecialchars("{$child['variationSize']}-{$child['variationColor']}");
                 $iwaskuList[] = "<span data-bs-toggle='tooltip' title='$tooltip'>{$child['iwasku']}</span>";
                 if (strlen($imageUrl) == 0) {
                     $imageUrl = $this->getThumbnail($child['imageUrl'] ?? '', 'katalog');
