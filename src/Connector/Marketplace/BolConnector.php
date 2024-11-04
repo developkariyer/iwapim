815a75d123bd551b12e283a239e7d8538e1605c2
enes <burdaenesvar@gmail.com>
2024-11-04 11:49:28 +0300

    a

--- src/Connector/Marketplace/BolConnector.php
+++ src/Connector/Marketplace/BolConnector.php
@@ -253,7 +253,7 @@ class BolConnector extends MarketplaceConnectorAbstract
         $this->prepareToken();
         $page = 1;
         $db = \Pimcore\Db::get();
-        $threeMonthsAgoTimestamp = strtotime('-1 months');
+        $threeMonthsAgoTimestamp = strtotime('-2 months');
         $threeMonthsAgoTimestamp = strtotime('+2 weeks');
         $threeMonthsAgo = date('Y-m-d', $threeMonthsAgoTimestamp);
         echo "Downloading orders from $threeMonthsAgo\n";
