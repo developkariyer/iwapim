404ac051993d74bf32ad0e39495693282b26f90b
enes <burdaenesvar@gmail.com>
2024-11-04 11:32:29 +0300

    a

--- src/Connector/Marketplace/BolConnector.php
+++ src/Connector/Marketplace/BolConnector.php
@@ -257,7 +257,7 @@ class BolConnector extends MarketplaceConnectorAbstract
             $params = ['status' => 'ALL', 'page' => $page, 'fulfilment-method' => 'ALL','latest-change-date'=>'2023-01-01'];
             $response = $this->httpClient->request("GET", static::$apiUrl['orders'], ['query' => $params]);
             if ($response->getStatusCode() !== 200) {
-                echo "Failed to download orders: " . $data->getContent() . "\n";
+                echo "Failed to download orders: " . $response->getContent() . "\n";
                 return;
             }
             
