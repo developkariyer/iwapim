b57f39f06f2abab4b4b66446ba4b1c84dbf3fcc6
enes <burdaenesvar@gmail.com>
2024-11-05 10:02:23 +0300

    a

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -12,6 +12,7 @@ use Pimcore\Model\DataObject\Marketplace;
 use Pimcore\Model\DataObject\Product;
 use App\Model\DataObject\VariantProduct;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use App\Utils\Utility;
 
 
 #[AsCommand(
