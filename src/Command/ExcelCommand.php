7b4b4a72e025ca2e456d6dab5108b40399a31513
developkariyer <develop@kariyerfora.com>
2024-11-05 11:23:19 +0100

    a

--- src/Command/ExcelCommand.php
+++ src/Command/ExcelCommand.php
@@ -57,7 +57,7 @@ class ExcelCommand extends AbstractCommand
         foreach ($products as $product) {
             $index++;
             echo "\rProcessing product $index {$product->getId()} ";
-            if ($product->level() != 1) {
+            if ($product->level()) {
                 continue;
             }
             $data[] = [
