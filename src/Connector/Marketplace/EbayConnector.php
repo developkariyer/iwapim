02e16d0a45681679783e42d6351b658047e21eea
enes <burdaenesvar@gmail.com>
2024-11-06 17:33:29 +0300

    a

--- src/Connector/Marketplace/EbayConnector.php
+++ src/Connector/Marketplace/EbayConnector.php
@@ -12,15 +12,44 @@ class EbayConnector extends MarketplaceConnectorAbstract
     private static $apiUrl = [
         'loginTokenUrl' => "https://api.ebay.com/identity/v1/oauth2/token"
     ];
+    private static $scopeList = [
+        'https://api.ebay.com/oauth/api_scope',
+        'https://api.ebay.com/oauth/api_scope/sell.marketing.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.marketing',
+        'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.inventory',
+        'https://api.ebay.com/oauth/api_scope/sell.account.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.account',
+        'https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.fulfillment',
+        'https://api.ebay.com/oauth/api_scope/sell.analytics.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.finances',
+        'https://api.ebay.com/oauth/api_scope/sell.payment.dispute',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.reputation',
+        'https://api.ebay.com/oauth/api_scope/sell.reputation.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription',
+        'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.stores',
+        'https://api.ebay.com/oauth/api_scope/sell.stores.readonly',
+        'https://api.ebay.com/oauth/api_scope'
+    ];
+    
     public static $marketplaceType = 'Ebay';
 
     protected function prepareToken()
     {
         if (!Utility::checkJwtTokenValidity($this->marketplace->getEbayAccessToken())) {
+            $scopeString = urlencode(implode(' ', $this->scopeList));
+
             $response = $this->httpClient->request('POST', static::$apiUrl['loginTokenUrl'], [
                 'headers' => [
                     'Authorization' => 'Basic ' . base64_encode("{$this->marketplace->getEbayClientId()}:{$this->marketplace->getEbayClientSecret()}"),
                     'Content-Type' => 'application/x-www-form-urlencoded'
+                ],
+                'form_params' => [
+                    'grant_type' => 'client_credentials',
+                    'scope' => scopeString
                 ]
             ]);
             print_r($response->getContent());
