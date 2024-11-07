98e6f4f2151883194ad4b01eeb00ffae75d854d1
enes <burdaenesvar@gmail.com>
2024-11-07 13:57:24 +0300

    a

--- src/Connector/Marketplace/TakealotConnector.php
+++ src/Connector/Marketplace/TakealotConnector.php
@@ -39,8 +39,7 @@ class TakealotConnector extends MarketplaceConnectorAbstract
                     echo "Error: $statusCode\n";
                     break;
                 }
-                print_r($response->getContent());
-                $data = $response->toArray();
+                $data = $response->getContent()->toArray();
                 $products = $data['offers'];
                 $this->listings = array_merge($this->listings, $products);
                 echo "Page: " . $page . " ";
