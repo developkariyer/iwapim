e285677bd51b5baee4d4403c002f4a46a473f398
enes <burdaenesvar@gmail.com>
2024-11-07 13:20:39 +0300

    a

--- src/Connector/Marketplace/TakealotConnector.php
+++ src/Connector/Marketplace/TakealotConnector.php
@@ -27,7 +27,7 @@ class TakealotConnector extends MarketplaceConnectorAbstract
             do {
                 $response = $this->httpClient->request('GET', static::$apiUrl['offers'], [
                     'headers' => [
-                        'Authorization' =>'Bearer ' . $this->marketplace->getTakealotKey()
+                        'Authorization' =>'Basic ' . $this->marketplace->getTakealotKey()
                     ],
                     'query' => [
                         'page_number' => $page,
