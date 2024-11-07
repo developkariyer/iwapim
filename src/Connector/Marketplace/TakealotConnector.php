d665c0510436a68a2121b491f27f9aa4422e2e30
enes <burdaenesvar@gmail.com>
2024-11-07 14:13:06 +0300

    a

--- src/Connector/Marketplace/TakealotConnector.php
+++ src/Connector/Marketplace/TakealotConnector.php
@@ -41,6 +41,7 @@ class TakealotConnector extends MarketplaceConnectorAbstract
                 }
                 $data = $response->toArray();
                 $products = $data['offers'];
+                print_r($products);
                 $this->listings = array_merge($this->listings, $products);
                 file_put_contents($filename,  json_encode($this->listings));
                 break;
