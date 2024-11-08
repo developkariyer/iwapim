189fe7d03572e478a96b6be15c88b9b9400bacef
enes <burdaenesvar@gmail.com>
2024-11-08 17:37:01 +0300

    a

--- src/Connector/Marketplace/WallmartConnector.php
+++ src/Connector/Marketplace/WallmartConnector.php
@@ -89,7 +89,7 @@ class WallmartConnector extends MarketplaceConnectorAbstract
                 $offset++;
                 echo ".";
                 sleep(1);  
-            } while ($data['totalItems'] === count($products));
+            } while ($limit === count($products));
             print_r($this->listings);
             //file_put_contents($filename, json_encode($this->listings));
         }
