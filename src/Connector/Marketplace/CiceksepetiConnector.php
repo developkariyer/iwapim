a7f9f058e271edb52c82eda43923926a9a02bb12
enes <burdaenesvar@gmail.com>
2024-11-13 17:27:22 +0300

    a

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -73,7 +73,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
                     $parent
                 );
             }
-            echo is_string($listing['mainProductCode']);
+            echo is_string($listing['mainProductCode']) ? 'true' : 'false';
             VariantProduct::addUpdateVariant(
                 variant: [
                     'imageUrl' => Utility::getCachedImage($listing['images'][0]) ?? '',
