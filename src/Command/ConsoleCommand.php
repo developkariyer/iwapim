e3452f2eb052ba325662712b484fe14a3df4f9c4
developkariyer <develop@kariyerfora.com>
2024-11-14 21:12:15 +0100

    a

--- src/Command/ConsoleCommand.php
+++ src/Command/ConsoleCommand.php
@@ -32,7 +32,7 @@ class ConsoleCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $amazon = new AmazonConnector(Marketplace::getById(200568));
-        $amazon->patchListing("LL-2P7Q-WAP7", "DE");
+        $amazon->patchListing("LL-2P7Q-WAP7", "UK");
 
         $io = new SymfonyStyle($input, $output);
         $io->title('IWAPIM Interactive Shell');
