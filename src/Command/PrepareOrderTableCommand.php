b8f864486750a1c0bdbcaaa6337e58b440260cf3
enes <burdaenesvar@gmail.com>
2024-11-15 17:34:06 +0300

    a

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -795,7 +795,7 @@ class PrepareOrderTableCommand extends AbstractCommand
         $db = \Pimcore\Db::get();
         $sql = "
         UPDATE iwa_marketplace_orders_line_items AS orders
-        JOIN iwa_currency_history AS history
+        JOIN iwa_currency_historyy AS history
         ON DATE(orders.created_at) = history.date
         SET orders.current_USD = history.usd, 
             orders.current_EUR = history.eur
