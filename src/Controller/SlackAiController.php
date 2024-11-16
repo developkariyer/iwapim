d940241ef217c66697141538e9cba4cce892d111
developkariyer <develop@kariyerfora.com>
2024-11-16 19:42:40 +0100

    a

--- src/Controller/SlackAiController.php
+++ src/Controller/SlackAiController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Messenger\MessageBusInterface;
 use App\SlackAi\SlackMessage;
 
-class SlackAi
+class SlackAiController
 {
     private MessageBusInterface $messageBus;
 
