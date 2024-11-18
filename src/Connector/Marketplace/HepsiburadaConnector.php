4185816a889539712f027521474666dc316054d0
enes <burdaenesvar@gmail.com>
2024-11-18 09:28:14 +0300

    a

--- src/Connector/Marketplace/HepsiburadaConnector.php
+++ src/Connector/Marketplace/HepsiburadaConnector.php
@@ -29,7 +29,7 @@ class HepsiburadaConnector extends MarketplaceConnectorAbstract
                 $response = $this->httpClient->request('GET', "https://listing-external.hepsiburada.com/listings/merchantid/{$this->marketplace->getSellerId()}", [
                     'headers' => [
                         'Authorization' => 'Basic ' . base64_encode($this->marketplace->getSellerId() . ':' . $this->marketplace->getServiceKey()),
-                        "User-Agent" => $this->marketplace->getSellerId()  . " - " . "SelfIntegration",
+                        "User-Agent" => "colorfullworlds_dev",
                         'Accept' => 'application/json',
                         'Content-Type' => 'application/json'
                     ],
