ff8fd6453c76f8435355d4f08fe76990dc04d7ff
enes <burdaenesvar@gmail.com>
2024-11-18 13:16:21 +0300

    a

--- src/Connector/Marketplace/EbayConnector.php
+++ src/Connector/Marketplace/EbayConnector.php
@@ -45,10 +45,10 @@ class EbayConnector extends MarketplaceConnectorAbstract
                 'scope' => "https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/sell.reputation https://api.ebay.com/oauth/api_scope/sell.reputation.readonly https://api.ebay.com/oauth/api_scope/commerce.notification.subscription https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly https://api.ebay.com/oauth/api_scope/sell.stores https://api.ebay.com/oauth/api_scope/sell.stores.readonly"
             ]
         ]);
-        print_r($response);
         if ($response->getStatusCode() !== 200) {
             throw new \Exception('Failed Ebay login');
         }
+        print_r($response->getContent());
     }
     
     
