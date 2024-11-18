2027574157e2772065a46b9f52d4578376cb3191
enes <burdaenesvar@gmail.com>
2024-11-18 13:26:22 +0300

    a

--- src/Connector/Marketplace/EbayConnector.php
+++ src/Connector/Marketplace/EbayConnector.php
@@ -12,26 +12,6 @@ class EbayConnector extends MarketplaceConnectorAbstract
     private static $apiUrl = [
         'loginTokenUrl' => "https://auth.ebay.com/oauth2/authorize"
     ];
-    private static $scopeList = [
-        'https://api.ebay.com/oauth/api_scope/sell.marketing.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.marketing',
-        'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.inventory',
-        'https://api.ebay.com/oauth/api_scope/sell.account.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.account',
-        'https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.fulfillment',
-        'https://api.ebay.com/oauth/api_scope/sell.analytics.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.finances',
-        'https://api.ebay.com/oauth/api_scope/sell.payment.dispute',
-        'https://api.ebay.com/oauth/api_scope/commerce.identity.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.reputation',
-        'https://api.ebay.com/oauth/api_scope/sell.reputation.readonly',
-        'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription',
-        'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.stores',
-        'https://api.ebay.com/oauth/api_scope/sell.stores.readonly'
-    ];
     
     public static $marketplaceType = 'Ebay';
 
