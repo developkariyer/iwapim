3607e3b8f0d3a84f85f27737191102e82701f9de
enes <burdaenesvar@gmail.com>
2024-11-18 08:58:34 +0300

    a

--- src/Connector/Marketplace/HepsiburadaConnector.php
+++ src/Connector/Marketplace/HepsiburadaConnector.php
@@ -26,9 +26,10 @@ class HepsiburadaConnector extends MarketplaceConnectorAbstract
             $limit = 10;
             $this->listings = [];
             //do {
-                $response = $this->httpClient->request('GET', "https://listing-external.hepsiburada.com/listings/merchantid/{$this->marketplace->getSellerId()}", [
+                $response = $this->httpClient->request('GET', "https://listing-external-sit.hepsiburada.com/listings/merchantid/{$this->marketplace->getSellerId()}", [
                     'headers' => [
-                        'Authorization' => 'Basic ' . base64_encode('colorfullworlds_dev' . ':' . $this->marketplace->getServiceKey()),
+                        'Authorization' => 'Basic ' . base64_encode($this->marketplace->getSellerId() . ':' . $this->marketplace->getServiceKey()),
+                        "User-Agent" => $this->marketplace->getSellerId()  . " - " . "SelfIntegration",
                         'Accept' => 'application/json',
                         'Content-Type' => 'application/json'
                     ],
