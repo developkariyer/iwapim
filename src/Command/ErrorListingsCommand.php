08316d112300a5b38e3a19c775848736ce1af98e
developkariyer <develop@kariyerfora.com>
2024-11-19 12:52:38 +0100

    a

--- src/Command/ErrorListingsCommand.php
+++ src/Command/ErrorListingsCommand.php
@@ -62,7 +62,7 @@ class ErrorListingsCommand extends AbstractCommand
         $amazonEuMarkets = ['DE', 'FR', 'IT', 'ES', 'NL', 'BE', 'SE', 'PL'];
         $variantObject = new VariantListing();
         $pageSize = 5;
-        $offset = 16597;
+        $offset = 16914;
         $variantObject->setLimit($pageSize);
         $variantObject->setUnpublished(false);
         $index = $offset;
