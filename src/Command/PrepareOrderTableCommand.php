f58a7daaaedc9f525e167e288a0661414e4def0c
enes <burdaenesvar@gmail.com>
2024-11-21 11:22:38 +0300

    a

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -50,7 +50,7 @@ class PrepareOrderTableCommand extends AbstractCommand
         if($input->getOption('extraColumns')) {
             $this->extraColumns();
         }
-        $this->currencyRate();
+        $this->calculatePrice();
         return Command::SUCCESS;
     }
     
