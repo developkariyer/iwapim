ab57f868aa486953a61171aab76684b8d27fa427
enes <burdaenesvar@gmail.com>
2024-11-27 08:28:23 +0300

    update TrendyolOrders

--- src/Connector/Marketplace/TrendyolConnector.php
+++ src/Connector/Marketplace/TrendyolConnector.php
@@ -64,8 +64,8 @@ class TrendyolConnector extends MarketplaceConnectorAbstract
              WHERE marketplace_id = ?",
             [$this->marketplace->getId()]
         );
-        //echo "Last Updated At: $lastUpdatedAt\n";
-        if ($lastUpdatedAt) {
+        echo "Last Updated At: $lastUpdatedAt\n";
+        /*if ($lastUpdatedAt) {
             $lastUpdatedAtTimestamp = strtotime($lastUpdatedAt);
             $threeMonthsAgo = strtotime('-3 months', $now);
             $startDate = max($lastUpdatedAtTimestamp, $threeMonthsAgo); 
@@ -143,7 +143,7 @@ class TrendyolConnector extends MarketplaceConnectorAbstract
                 return implode('-', $values);
             }
         }
-        return '';
+        return '';*/
     }
 
     private function getPublished($listing)
