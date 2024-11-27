790fc5f50738073b6de1aa4e0d7940768c19271e
enes <burdaenesvar@gmail.com>
2024-11-27 14:00:39 +0300

    CiceksepetiOrder

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -132,7 +132,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
                 ]
             ]);
             $statusCode = $response->getStatusCode();
-            print_r($response);
+            print_r($response->getContent());
             /*if ($statusCode !== 200) {
                 echo "Error: $statusCode\n";
                 return;
