1768319056ade67508996afb30b2a9ff0ec35945
developkariyer <develop@kariyerfora.com>
2024-11-29 00:36:15 +0100

    amazon order test

--- src/Connector/Marketplace/Amazon/Orders.php
+++ src/Connector/Marketplace/Amazon/Orders.php
@@ -21,15 +21,21 @@ class Orders
         $ordersApi = $this->amazonConnector->amazonSellerConnector->ordersV0();
         $marketplaceIds = [AmazonConstants::amazonMerchant[$this->amazonConnector->mainCountry]['id']];
         $nextToken = null;
+        $orders = [];
+        $lastUpdatedAfter = gmdate('Y-m-d\TH:i:s\Z', strtotime('-1 week'));
+        echo "lastUpdatedAfter: $lastUpdatedAfter\n";
         do {
-            $lastUpdatedAfter = gmdate('Y-m-d\TH:i:s\Z', strtotime('-1 day'));
-            echo "lastUpdatedAfter: $lastUpdatedAfter\n";
-            $response = $ordersApi->getOrders(marketplaceIds: $marketplaceIds, lastUpdatedAfter: $lastUpdatedAfter);
-            file_put_contents(PIMCORE_PROJECT_ROOT."/tmp/amazon_order_test.txt", json_encode($response->json()));
-            return;
-//            $nextToken = $response->getNextToken();
-//            $orders = array_merge($orders, $response->getOrders());
+            $response = $nextToken ? $ordersApi->getOrders(marketplaceIds: $marketplaceIds, nextToken: $nextToken) : $ordersApi->getOrders(marketplaceIds: $marketplaceIds, lastUpdatedAfter: $lastUpdatedAfter);
+            $orders = array_merge($orders, $response->getOrders());
+            $nextToken = $response->getNextToken();
+            if ($nextToken) {
+                echo "Next Token: $nextToken\n";
+            } else {
+                echo "No Next Token\n";
+            }
         } while ($nextToken);
+        echo "Total Orders: " . count($orders) . "\n";
+        file_put_contents(PIMCORE_PROJECT_ROOT . "/tmp/amazon-orders.json", json_encode($orders));
     }
 
 }
