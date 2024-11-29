32f26a8880d38d71cef2c413ef643ce21eebf2dc
developkariyer <develop@kariyerfora.com>
2024-11-30 00:16:11 +0100

    iwabot debug

--- src/Connector/IwabotConnector.php
+++ src/Connector/IwabotConnector.php
@@ -13,6 +13,7 @@ class IwabotConnector
         file_put_contents(PIMCORE_PROJECT_ROOT . "/tmp/iwabot.csv", $report);
         $lines = explode("\n", mb_convert_encoding(trim($report), 'UTF-8', 'UTF-8'));
         $header = str_getcsv(array_shift($lines), "\t");
+        print_r($header);
         foreach ($lines as $line) {
             $data = str_getcsv($line, "\t");
             if (count($header) == count($data)) {
