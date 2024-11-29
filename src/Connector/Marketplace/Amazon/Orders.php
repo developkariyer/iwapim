c11c2bcd37a254c022a63685cb6425d1b9d3f93a
developkariyer <develop@kariyerfora.com>
2024-11-29 13:07:31 +0100

    Amazon order test

--- src/Connector/Marketplace/Amazon/Orders.php
+++ src/Connector/Marketplace/Amazon/Orders.php
@@ -11,6 +11,7 @@ class Orders
     public $amazonConnector;
 
     public $db;
+    public $orders = [];
 
     public function __construct(AmazonConnector $amazonConnector) 
     {
@@ -20,19 +21,28 @@ class Orders
 
     public function getLastUpdateTime()
     {
-        $query = "SELECT MAX(last_updated) as last_updated FROM amazon_orders";
-        $stmt = $this->db->query($query);
-        $result = $stmt->fetch();
-        return $result['last_updated'];
+        $lastUpdateTime = $this->db->fetchOne(
+            "SELECT CONCAT(
+                DATE_FORMAT(
+                    MAX(STR_TO_DATE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.LastUpdateDate')), '%Y-%m-%dT%H:%i:%sZ')),
+                    '%Y-%m-%dT%H:%i:%s'
+                ),
+                'Z'
+            ) AS lastUpdatedAt
+            FROM iwa_marketplace_orders
+            WHERE marketplace_id = ?",
+            [$this->amazonConnector->getMarketplace()->getId()]
+        );
+        return $lastUpdateTime ?? gmdate('Y-m-d\TH:i:s\Z', strtotime('-1 day'));
     }
 
-    public function downloadOrders()
+    public function getOrders()
     {
         $ordersApi = $this->amazonConnector->amazonSellerConnector->ordersV0();
         $marketplaceIds = [AmazonConstants::amazonMerchant[$this->amazonConnector->mainCountry]['id']];
         $nextToken = null;
         $orders = [];
-        $lastUpdatedAfter = gmdate('Y-m-d\TH:i:s\Z', strtotime('-10 years'));
+        $lastUpdatedAfter = $this->getLastUpdateTime();
         echo "lastUpdatedAfter: $lastUpdatedAfter\n";
         $rateLimitSleep = 60;
         $burstLimit = 20;
@@ -60,7 +70,41 @@ class Orders
             }
         } while ($nextToken);
         echo "Total Orders: " . count($orders) . "\n";
-        file_put_contents(PIMCORE_PROJECT_ROOT . "/tmp/amazon-orders.json", json_encode($orders));
+        $this->orders = $orders;
+    }
+
+    public function getOrderItems()
+    {
+
+    }
+
+    public function saveOrders()
+    {
+        $this->db->beginTransaction();
+        try {
+            foreach ($this->orders as $order) {
+                $this->db->executeStatement(
+                    "INSERT INTO iwa_marketplace_orders (marketplace_id, order_id, json) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE json = ?",
+                    [
+                        $this->amazonConnector->getMarketplace()->getId(),
+                        $order['AmazonOrderId'],
+                        json_encode($order),
+                        json_encode($order)
+                    ]
+                );
+            }
+            $this->db->commit();
+        } catch (\Exception $e) {
+            $this->db->rollBack();
+            throw $e;
+        }
+    }
+
+    public function downloadOrders()
+    {
+        $this->getOrders();
+        $this->getOrderItems();
+        $this->saveOrders();
     }
 
 }
