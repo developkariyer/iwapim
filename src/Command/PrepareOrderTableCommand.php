dcf5676bbc62f0b6816cd45d51df120811733e03
enesbt <burdaenesvar@gmail.com>
2024-12-02 11:02:20 +0300

    Amaon order line items

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -445,7 +445,7 @@ class PrepareOrderTableCommand extends AbstractCommand
             SELECT 
                 DISTINCT variant_id
             FROM
-                iwa_marketplace_orders_line_items
+                iwa_amazon_orders_line_items
             WHERE 
                 marketplace_type = '$marketplaceType'
             ";
