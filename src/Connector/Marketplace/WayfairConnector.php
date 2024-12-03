a16e8432c1175fc838757c56b75318151f04e9c1
enesbt <burdaenesvar@gmail.com>
2024-12-03 17:03:59 +0300

    Wayfair API Sandbox Testing

--- src/Connector/Marketplace/WayfairConnector.php
+++ src/Connector/Marketplace/WayfairConnector.php
@@ -10,7 +10,7 @@ class WayfairConnector extends MarketplaceConnectorAbstract
 {
     private static $apiUrl = [
         'oauth' => 'https://sso.auth.wayfair.com/oauth/token',
-        'orders' => 'https://sandbox.api.wayfair.com/graphql/orders',
+        'orders' => 'https://sandbox.api.wayfair.com/v1/graphql',
     ];
     public static $marketplaceType = 'Wayfair';
     public static $expires_in;
