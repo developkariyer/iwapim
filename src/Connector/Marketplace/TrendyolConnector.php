702cd4f870ce0d1deb4ce52959ccc09dbb6f8f4a
enesbt <burdaenesvar@gmail.com>
2024-12-04 15:42:20 +0300

    Trendyol setInventory Function Test

--- src/Connector/Marketplace/TrendyolConnector.php
+++ src/Connector/Marketplace/TrendyolConnector.php
@@ -18,7 +18,7 @@ class TrendyolConnector extends MarketplaceConnectorAbstract
         
         if ($this->marketplace->getKey() === 'TrendyolIwa') {
             $variantProduct = VariantProduct::getById(194109);
-            print_r($variantProduct->json_decode($listing->jsonRead('apiResponseJson'), true)['barcode']); 
+            print_r($variantProduct->json_decode($variantProduct->jsonRead('apiResponseJson'), true)['barcode']); 
             //$this->setInventory();
         }
         
