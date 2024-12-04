3bece43444a4ef909da2538af3e215f58b690bdb
enesbt <burdaenesvar@gmail.com>
2024-12-04 09:17:02 +0300

    Wayfair API Sandbox Testing

--- src/Connector/Marketplace/WayfairConnector.php
+++ src/Connector/Marketplace/WayfairConnector.php
@@ -85,7 +85,7 @@ class WayfairConnector extends MarketplaceConnectorAbstract
         GRAPHQL;
         $variables = [
             'notice' => [
-                'poNumber' => 'TEST_68895983',
+                'poNumber' => 'TEST_23082207',
                 'supplierId' => 194115,
                 'packageCount' => 1,
                 'weight' => 184,
@@ -114,7 +114,7 @@ class WayfairConnector extends MarketplaceConnectorAbstract
                 ],
                 'largeParcelShipments' => [
                     [
-                        'partNumber' => '2SIZEWLLMNTDESK',
+                        'partNumber' => '4KULKUFICINGOLDS70',
                         'packages' => [
                             [
                                 'code' => [
