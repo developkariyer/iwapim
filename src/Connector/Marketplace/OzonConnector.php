fb443193e59fe0c157d181f43102c8136a34cf73
enesbt <burdaenesvar@gmail.com>
2024-12-05 12:55:52 +0300

    OzonConnector Added

---
+++ src/Connector/Marketplace/OzonConnector.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Connector\Marketplace;
+
+use Pimcore\Model\DataObject\VariantProduct;
+use App\Utils\Utility;
+
+class OzonConnector extends MarketplaceConnectorAbstract
+{
+    public static $marketplaceType = 'Ozon';
+
+    public function download($forceDownload = false)
+    {
+       
+    }
+
+    public function downloadInventory()
+    {
+        
+    }
+
+    public function downloadOrders()
+    {
+
+    }
+
+    public function import($updateFlag, $importFlag)
+    {
+
+    }
+
+}
