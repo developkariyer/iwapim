ddfaec10efd06bd9ac2296ebaddc4ffae835fd04
enesbt <burdaenesvar@gmail.com>
2024-12-05 14:59:30 +0300

    OzonConnector Download

--- src/Connector/Marketplace/OzonConnector.php
+++ src/Connector/Marketplace/OzonConnector.php
@@ -41,7 +41,7 @@ class OzonConnector extends MarketplaceConnectorAbstract
         }
         $data = $response->toArray();
         $products = $data['result']['items'];
-        print_r($response);
+        print_r($response->getContent());
         /*foreach ($products as &$product) {
             $detail = $this->httpClient->request('POST', "https://api-seller.ozon.ru/v2/product/info", [
                 'headers' => [
