6eaf489befa21945a0131ba086ae68a77dfbb9a8
enesbt <burdaenesvar@gmail.com>
2024-12-10 13:08:47 +0300

    update Ciceksepeti setInventory & setPrice

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -2,7 +2,7 @@
 
 namespace App\Connector\Marketplace;
 
-use Pimcore\Model\DataObject\VariantProduct;
+use App\Model\DataObject\VariantProduct;
 use App\Utils\Utility;
 use Symfony\Component\HttpClient\HttpClient;
 
@@ -18,11 +18,11 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
     
     public function download($forceDownload = false)
     {
-        $variant = VariantProduct::getById(266259);
+        $variant = VariantProduct::getById(266097);
         print_r($variant);
         //$this->setInventory($variant, 299);
         //$this->setPrice($variant, 1150);
-
+        
         /*$this->listings = json_decode(Utility::getCustomCache('LISTINGS.json', PIMCORE_PROJECT_ROOT. "/tmp/marketplaces/".urlencode($this->marketplace->getKey())), true);
         if (!(empty($this->listings) || $forceDownload)) {
             echo "Using cached listings\n";
