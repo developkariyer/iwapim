1abe39422d153615cbdb9fa9b1c7945ee07cbe0a
enesbt <burdaenesvar@gmail.com>
2024-12-10 16:38:22 +0300

    Bol.com setPrice & setInventory Integration

--- src/Connector/Marketplace/BolConnector.php
+++ src/Connector/Marketplace/BolConnector.php
@@ -207,8 +207,8 @@ class BolConnector extends MarketplaceConnectorAbstract
     public function download($forceDownload = false)
     {
         $variant = VariantProduct::getById(218571);
-        //$this->setInventory($variant, 5);
-        $this->setPrice($variant, '100');        
+        $this->setInventory($variant, 7);
+        //$this->setPrice($variant, '100');        
         /*$this->listings = json_decode(Utility::getCustomCache('BOL_LISTINGS.json', PIMCORE_PROJECT_ROOT. "/tmp/marketplaces/".urlencode($this->marketplace->getKey())), true);
         if (!(empty($this->listings) || $forceDownload)) {
             echo "Using cached listings\n";
