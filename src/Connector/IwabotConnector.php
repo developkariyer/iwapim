21af35109f812deaad1fb2c9ea0ac683493ddb56
Umut Demirhan <develop@kariyerfora.com>
2024-12-10 23:15:22 +0100

    phpstorm migration test

--- src/Connector/IwabotConnector.php
+++ src/Connector/IwabotConnector.php
@@ -3,21 +3,26 @@
 namespace App\Connector;
 
 use App\Utils\Registry;
+use Doctrine\DBAL\Exception;
+use Pimcore\Db;
 
 class IwabotConnector
 {
-    public static function downloadReport()
+    /**
+     * @throws Exception
+     */
+    public static function downloadReport(): void
     {
-        $db = \Pimcore\Db::get();
+        $db = Db::get();
         $sql = "INSERT INTO iwa_inventory (inventory_type, warehouse, asin, fnsku, iwasku, json_data, total_quantity, created_updated_at) VALUES ('IWA', 'NJ', ?, ?, ?, ?, ?, NOW()) ON DUPLICATE KEY UPDATE iwasku = ?, json_data = ?, total_quantity = ?, created_updated_at = NOW()";
         $report = file_get_contents('https://iwarden.iwaconcept.com/iwabot/warehouse/report.php?csv=1');
         file_put_contents(PIMCORE_PROJECT_ROOT . "/tmp/iwabot.csv", $report);
         $lines = explode("\n", mb_convert_encoding(trim($report), 'UTF-8', 'UTF-8'));
-        $header = str_getcsv(array_shift($lines), ",");
+        $header = str_getcsv(array_shift($lines));
         $db->beginTransaction();
         try {
             foreach ($lines as $line) {
-                $data = str_getcsv($line, ",");
+                $data = str_getcsv($line);
                 if (count($header) == count($data)) {
                     $rowData = array_combine($header, $data);
                     $fnsku = $rowData['FNSKU'] ?? null;
