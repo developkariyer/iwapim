f0c0bc3c543d0871f0c1fd90f75563434890cd48
enesbt <burdaenesvar@gmail.com>
2024-12-11 10:11:46 +0300

    Ciceksepeti Orders

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -123,7 +123,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
         $now = date('Y-m-d'); 
         $lastUpdatedAt = $db->fetchOne(
             "SELECT COALESCE(
-                DATE_FORMAT(MAX(STR_TO_DATE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.orderModifyDate')), '%d-%m-%Y')), '%Y-%m-%d'),
+                DATE_FORMAT(MAX(STR_TO_DATE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.orderModifyDate')), '%d/%m/%Y')), '%Y-%m-%d'),
                 DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 3 MONTH), '%Y-%m-%d')
             ) AS lastUpdatedAt
             FROM iwa_marketplace_orders
