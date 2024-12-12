967623c5c104773064c7792036a5358321aa97c0
Umut Demirhan <develop@kariyerfora.com>
2024-12-12 19:16:35 +0100

    sql queries for amazon related temporary tables

--- src/SQL/iwa_amazon_daily_sales_summary.sql
+++ src/SQL/iwa_amazon_daily_sales_summary.sql
@@ -16,7 +16,7 @@ WITH RECURSIVE
                           JSON_UNQUOTE(JSON_EXTRACT(o.json, '$.SalesChannel'))                                      AS sales_channel,
                           o.order_id
                    FROM iwa_marketplace_orders o
-                   WHERE REGEXP_LIKE(o.order_id, '^[0-9]{3}-[0-9]{7}-[0-9]{7}$')
+                   WHERE REGEXP_LIKE(o.order_id, '^[0-9]{3}-[0-9]{7}-[0-9]{7}$') -- Validate Amazon Order ID format
                      AND JSON_UNQUOTE(JSON_EXTRACT(o.json, '$.OrderStatus')) = 'Shipped'),
     expanded_sales AS (SELECT JSON_UNQUOTE(JSON_EXTRACT(s.asin_array, CONCAT('$[', idx.n, ']')))         AS asin,
                               CAST(JSON_EXTRACT(s.quantity_array, CONCAT('$[', idx.n, ']')) AS UNSIGNED) AS quantity_shipped,
@@ -24,25 +24,31 @@ WITH RECURSIVE
                               s.sales_channel,
                               s.order_id
                        FROM sales_data s
-                                JOIN idx
-                                     ON idx.n < JSON_LENGTH(s.asin_array)),
+                                JOIN
+                            idx
+                            ON
+                                idx.n < JSON_LENGTH(s.asin_array)),
     daily_sales AS (SELECT asin,
                            sales_channel,
     DATE
 (
     purchase_date
-) AS sale_date, -- Removed second argument from DATE()
+) AS sale_date, -- Extract only the date
     SUM
 (
     quantity_shipped
 ) AS total_quantity
-    FROM expanded_sales
-    WHERE purchase_date >= DATE_SUB
+    FROM
+    expanded_sales
+    WHERE
+    purchase_date >= DATE_SUB
 (
     CURDATE
 (
-), INTERVAL 80 WEEK)
-    GROUP BY asin, sales_channel, sale_date)
+), INTERVAL 80 WEEK) -- Past 80 weeks
+    GROUP BY
+    asin, sales_channel, sale_date
+    )
 SELECT asin,
        sales_channel,
        sale_date,
