465914af31e9c56804294c480fbbb5663f5b4d63
Umut Demirhan <develop@kariyerfora.com>
2024-12-12 21:19:55 +0100

    add python to pimcore for forecast operations using Prophet

--- src/Python/iwa_sales_forecast.py
+++ src/Python/iwa_sales_forecast.py
@@ -5,6 +5,7 @@ from database_operations import fetch_pairs, fetch_data, insert_forecast_data
 from forecast_generator import generate_forecast
 
 logging.getLogger().setLevel(logging.WARNING)
+logging.getLogger("cmdstanpy").setLevel(logging.WARNING)
 
 def run_forecast_pipeline(yaml_path):
     """
