9b92cb871a3e61ea3e78e3048f4d2306b4d586a3
Umut Demirhan <develop@kariyerfora.com>
2024-12-12 23:59:23 +0100

    chart demo

--- src/Controller/WarehouseController.php
+++ src/Controller/WarehouseController.php
@@ -87,6 +87,20 @@ class WarehouseController extends FrontendController
         );
 
         $response = [
+            'lastYearLast90' => 0,
+            'lastYearLast30' => 0,
+            'lastYearLast7' => 0,
+            'lastYearNext7' => 0,
+            'lastYearNext30' => 0,
+            'lastYearNext90' => 0,
+            'lastYearTotal' => 0,
+            'last90' => 0,
+            'last30' => 0,
+            'last7' => 0,
+            'next7' => 0,
+            'next30' => 0,
+            'next90' => 0,
+            'nextTotal' => 0,
             'xAxisLabels' => [],
             'previousYearData' => array_fill(0, 53, null),
             'currentData' => array_fill(0, 53, null),
@@ -99,25 +113,43 @@ class WarehouseController extends FrontendController
 
         foreach ($salesData as $data) {
             $date = new DateTime($data['sale_date']);
+            $days = (int) $endCurrentData->diff($date)->format('%r%a');
             $week = (int) $startPreviousYearData->diff($date)->format('%r%a') / 7;
+            $quantity = (int) $data['total_quantity'];
+
+            $response['lastYearLast90'] += ($days <= -365 && $days > -365-90) ? $quantity : 0;
+            $response['lastYearLast30'] += ($days <= -365 && $days > -365-30) ? $quantity : 0;
+            $response['lastYearLast7'] += ($days <= -365 && $days > -365-7) ? $quantity : 0;
+            $response['lastYearNext7'] += ($days > -365 && $days <= -365+7) ? $quantity : 0;
+            $response['lastYearNext30'] += ($days > -365 && $days <= -365+30) ? $quantity : 0;
+            $response['lastYearNext90'] += ($days > -365 && $days <= -365+90) ? $quantity : 0;
+            $response['lastYearTotal'] += ($days < -182) ? $quantity : 0;
+            $response['last90'] += ($days > -90 && $days <= 0) ? $quantity : 0;
+            $response['last30'] += ($days > -30 && $days <= 0) ? $quantity : 0;
+            $response['last7'] += ($days > -7 && $days <= 0) ? $quantity : 0;
+            $response['next7'] += ($days > 0 && $days <= 7) ? $quantity : 0;
+            $response['next30'] += ($days > 0 && $days <= 30) ? $quantity : 0;
+            $response['next90'] += ($days > 0 && $days <= 90) ? $quantity : 0;
+            $response['nextTotal'] += ($days >= -182) ? $quantity : 0;
+
             if ($week < 53) {
                 if (is_null($response['previousYearData'][$week])) {
                     $response['previousYearData'][$week] = 0;
                 }
-                $response['previousYearData'][$week] += (int) $data['total_quantity'];
+                $response['previousYearData'][$week] += $quantity;
                 continue;
             }
             if ($week < 79) {
                 if (is_null($response['currentData'][$week - 53])) {
                     $response['currentData'][$week - 53] = 0;
                 }
-                $response['currentData'][$week - 53] += (int) $data['total_quantity'];
+                $response['currentData'][$week - 53] += $quantity;
                 continue;
             }
             if (is_null($response['forecastedData'][$week - 53])) {
                 $response['forecastedData'][$week - 53] = 0;
             }
-            $response['forecastedData'][$week - 53] += (int) $data['total_quantity'];
+            $response['forecastedData'][$week - 53] += $quantity;
         }
 
         $response['currentData'][26] = $response['forecastedData'][26];
