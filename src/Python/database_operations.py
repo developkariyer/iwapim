f958f3696d570d4814ceafd7445bc28b7f624f20
Umut Demirhan <develop@kariyerfora.com>
2024-12-12 21:06:24 +0100

    add python to pimcore for forecast operations using Prophet

--- src/Python/database_operations.py
+++ src/Python/database_operations.py
@@ -61,6 +61,9 @@ def fetch_pairs(yaml_path):
             engine.dispose()
 
 
+from sqlalchemy import create_engine, text
+from db_config_loader import get_mysql_config
+
 def insert_forecast_data(forecast_data, asin, sales_channel, yaml_path):
     """
     Inserts or updates forecasted data into the MySQL `iwa_amazon_daily_sales_summary` table.
@@ -97,17 +100,17 @@ def insert_forecast_data(forecast_data, asin, sales_channel, yaml_path):
         rows_to_insert = list(forecast_data[['asin', 'sales_channel', 'sale_date', 'total_quantity', 'data_source']].itertuples(index=False, name=None))
 
         # SQL query for INSERT or UPDATE
-        insert_query = """
+        insert_query = text("""
         INSERT INTO iwa_amazon_daily_sales_summary (asin, sales_channel, sale_date, total_quantity, data_source)
-        VALUES (%s, %s, %s, %s, %s)
+        VALUES (:asin, :sales_channel, :sale_date, :total_quantity, :data_source)
         ON DUPLICATE KEY UPDATE
             total_quantity = VALUES(total_quantity),
             data_source = VALUES(data_source)
-        """
+        """)
 
         # Execute batch insert/update
         with connection.begin() as transaction:
-            connection.execute(insert_query, rows_to_insert)
+            connection.execute(insert_query, [{'asin': row[0], 'sales_channel': row[1], 'sale_date': row[2], 'total_quantity': row[3], 'data_source': row[4]} for row in rows_to_insert])
 
         print(f"Inserted/Updated forecast data for ASIN {asin} and Sales Channel {sales_channel}.")
 
