4238b21c76df84329fbe32e823d6b3fed7b6e600
Umut Demirhan <develop@kariyerfora.com>
2024-12-18 01:54:49 +0100

    Marketplace connectors fixes and Ozon connector creation

--- src/Connector/Marketplace/Ozon/Products.php
+++ src/Connector/Marketplace/Ozon/Products.php
@@ -53,13 +53,9 @@ class Products
     {
         echo "  Processing category tree\n";
         $db = Db::get();
-        echo "1";
         $db->executeQuery("DELETE FROM " . self::OZON_CATEGORY_TABLE);
-        echo "2";
         $db->executeQuery("DELETE FROM " . self::OZON_PRODUCTTYPE_TABLE);
-        echo "3";
         $db->beginTransaction();
-        echo "4";
         try {
             $stack = [[
                 'parentId' => null,
@@ -67,7 +63,6 @@ class Products
             ]];
             echo count($stack) . " ". count($categoryTree) . "\n";
             while (!empty($stack)) {
-                echo "TEST\n";
                 $current = array_pop($stack);
                 $currentParentId = $current['parentId'];
                 $currentChildren = $current['children'];
