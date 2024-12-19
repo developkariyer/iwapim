d55eac6bf7b1027c5c7915db444acdc8dd6f3f61
enesbt <burdaenesvar@gmail.com>
2024-12-19 11:17:25 +0300

    Shopify abondoned Checkouts

--- src/Connector/Marketplace/ShopifyConnector.php
+++ src/Connector/Marketplace/ShopifyConnector.php
@@ -30,6 +30,7 @@ class ShopifyConnector extends MarketplaceConnectorAbstract
         if (!str_contains($this->apiUrl, 'https://')) {
             $this->apiUrl = "https://{$this->apiUrl}/admin/api/2024-07";
         }
+        $this->downloadAbondonedCheckouts();
     }
 
     /**
