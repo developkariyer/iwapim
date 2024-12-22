ed5776dbadab6e3e66ccfadf6011018b19685fa3
Umut Demirhan <develop@kariyerfora.com>
2024-12-22 01:57:35 +0100

    ozon frontend

--- src/Controller/OzonController.php
+++ src/Controller/OzonController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Pimcore\Model\DataObject\Marketplace;
+use Pimcore\Model\DataObject\ListingTemplate;
 
 class OzonController extends FrontendController
 {
@@ -34,9 +35,20 @@ class OzonController extends FrontendController
         $mrkListing = new Marketplace\Listing();
         $mrkListing->setCondition("marketplaceType = ?", ['Ozon']);
         $marketplaces = $mrkListing->load();
-        $tasks = [
-            ['id' => 1, 'title' => 'Task 1']
-        ];
+
+        $taskListing = new ListingTemplate\Listing();
+        $tasksObjects = $taskListing->load();
+
+        $tasks = [];
+        foreach ($tasksObjects as $task) {
+            if ($task->getMarketplace()->getMarketplaceType() !== 'Ozon') {
+                continue;
+            }
+            $tasks[] = [
+                'id' => $task->getId(),
+                'title' => $task->getKey(),
+            ];
+        }
 
         return $this->render('ozon/ozon.html.twig', [
             'tasks' => $tasks,
@@ -45,24 +57,41 @@ class OzonController extends FrontendController
     }
 
     /**
-     * @Route("/ozon/category-tree", name="ozon_category_tree")
+     * @Route("/ozon/task/{id}", name="ozon_task")
+     * @param Request $request
+     * @param int $id
+     * @return Response
      */
-    public function categoryTreeAction(Request $request): JsonResponse
+    public function taskAction(Request $request, int $id): Response
     {
-        $categories = [
-            ['id' => 1, 'name' => 'Electronics', 'children' => [
-                ['id' => 2, 'name' => 'Laptops'],
-                ['id' => 3, 'name' => 'Smartphones'],
-            ]],
-            ['id' => 4, 'name' => 'Fashion', 'children' => [
-                ['id' => 5, 'name' => 'Men'],
-                ['id' => 6, 'name' => 'Women'],
-            ]],
-        ];
+        $task = ListingTemplate::getById($id);
+        if (!$task) {
+            return new Response('Task not found');
+        }
+        return $this->render('ozon/task.html.twig', [
+            'task' => $task,
+        ]);
+    }
 
-        $categories = json_decode(Utility::getCustomCache('CATEGORY_TREE.json', PIMCORE_PROJECT_ROOT. "/tmp/marketplaces/Ozon"), true) ?? $categories;
-
-        return new JsonResponse($categories);
+    /**
+     * @Route("/ozon/newtask", name="ozon_newtask_action")
+     * @param Request $request
+     *
+     * This controller method creates a new ListingTemplate and redirects page to /ozon/task/{id} page
+     * @throws Exception
+     */
+    public function newTaskAction(Request $request)
+    {
+        $task = new ListingTemplate();
+        // get key from request POST
+        $task->setKey($request->get('taskName', 'İsimsiz'));
+        $marketplaceId = $request->get('marketplace', 0);
+        if (!$marketplaceId) {
+            return new JsonResponse(['error' => 'Marketplace not found'], 400);
+        }
+        $task->setMarketplace(Marketplace::getById($marketplaceId) ?? null);
+        $task->save();
+        return $this->redirectToRoute('ozon_task', ['id' => $task->getId()]);
     }
 
 }
