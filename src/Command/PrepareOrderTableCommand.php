3f2d5376ad63b50cdbb92c072580061d12cbbbaa
enesbt <burdaenesvar@gmail.com>
2024-12-26 13:02:41 +0300

    calculateprice added utility

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -64,7 +64,7 @@ class PrepareOrderTableCommand extends AbstractCommand
 
     protected function test()
     {
-        $result = Utility::convertCurrency("500", "TL", "USD","2024-12-24");
+        $result = Utility::convertCurrency("500", "EUR", "USD","2024-12-24");
         echo $result;
     }
 
