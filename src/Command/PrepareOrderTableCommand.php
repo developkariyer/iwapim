2b314d36e76d13a36769eb6edb29b1b3f2671128
enesbt <burdaenesvar@gmail.com>
2024-12-26 16:06:04 +0300

    updated extraColumns

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -474,65 +474,14 @@ class PrepareOrderTableCommand extends AbstractCommand
 
     protected function usaCode(): void
     {
-        $isoCodes = [
-            'Alabama' => 'US-AL',
-            'Alaska' =>'US-AK',
-            'Arizona' =>'US-AZ',
-            'Arkansas' =>'US-AR',
-            'California' =>'US-CA',
-            'Colorado' =>'US-CO',
-            'Connecticut' =>'US-CT',
-            'Delaware' =>'US-DE',
-            'Florida' =>'US-FL',
-            'Georgia' =>'US-GA',
-            'Hawaii' =>'US-HI',
-            'Idaho' =>'US-ID',
-            'Illinois' =>'US-IL',
-            'Indiana' =>'US-IN',
-            'Iowa' =>'US-IA',
-            'Kansas' =>'US-KS',
-            'Kentucky' =>'US-KY',
-            'Louisiana' =>'US-LA',
-            'Maine' =>'US-ME',
-            'Maryland' =>'US-MD',
-            'Massachusetts' =>'US-MA',
-            'Michigan' =>'US-MI',
-            'Minnesota' =>'US-MN',
-            'Mississippi' =>'US-MS',
-            'Missouri' =>'US-MO',
-            'Montana' =>'US-MT',
-            'Nebraska' =>'US-NE',
-            'Nevada' =>'US-NV',
-            'New Hampshire' =>'US-NH',
-            'New Jersey' =>'US-NJ',
-            'New Mexico' =>'US-NM',
-            'New York' =>'US-NY',
-            'North Carolina' =>'US-NC',
-            'North Dakota' =>'US-ND',
-            'Ohio' =>'US-OH',
-            'Oklahoma' =>'US-OK',
-            'Oregon' =>'US-OR',
-            'Pennsylvania' =>'US-PA',
-            'Rhode Island' =>'US-RI',
-            'South Carolina' =>'US-SC',
-            'South Dakota' =>'US-SD',
-            'Tennessee' =>'US-TN',
-            'Texas' =>'US-TX',
-            'Utah' =>'US-UT',
-            'Vermont' =>'US-VT',
-            'Virginia' =>'US-VA',
-            'Washington' =>'US-WA',
-            'West Virginia' =>'US-WV',
-            'Wisconsin' =>'US-WI',
-            'Wyoming' =>'US-WY',
-            'American Samoa' =>'US-AS',
-            'Guam' =>'US-GU',
-            'Northern Mariana Islands' =>'US-MP',
-            'Puerto Rico' =>'US-PR',
-            'United States Minor Outlying Islands' =>'US-UM',
-            'Virgin Islands, U.S.' =>'US-VI',
-            'District of Columbia' =>'US-DC',
-        ];
+        $filePath = PIMCORE_PROJECT_ROOT . '/src/JSON/usa_states.json';
+        if (!file_exists($filePath)) {
+            throw new Exception("USA states JSON file not found.");
+        }
+        $isoCodes = json_decode(file_get_contents($filePath), true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception("Error parsing JSON file: " . json_last_error_msg());
+        }
         $db = \Pimcore\Db::get();
         $sql = "
             SELECT DISTINCT shipping_province 
