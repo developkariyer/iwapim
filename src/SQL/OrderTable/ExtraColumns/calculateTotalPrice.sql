412ee5a1845d0bed37e776605fc5efa8bb8742fd
enesbt <burdaenesvar@gmail.com>
2024-12-27 11:04:38 +0300

    updated prepareOrderTable

--- src/SQL/OrderTable/ExtraColumns/calculateTotalPrice.sql
+++ src/SQL/OrderTable/ExtraColumns/calculateTotalPrice.sql
@@ -4,9 +4,9 @@ UPDATE iwa_marketplace_orders_line_items AS t1
     order_id,
     SUM(price) AS total_price
     FROM iwa_marketplace_orders_line_items
-    WHERE marketplace_type = :marketplace_type
+    WHERE marketplace_type = :marketplaceType
     GROUP BY order_id
     ) AS t2
 ON t1.order_id = t2.order_id
     SET t1.total_price = t2.total_price
-WHERE t1.marketplace_type = :marketplace_type;
+WHERE t1.marketplace_type = :marketplaceType;
