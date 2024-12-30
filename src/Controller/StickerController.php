65d48b76cd4125191e600f59e25102743763fcc3
enesbt <burdaenesvar@gmail.com>
2024-12-30 15:37:40 +0300

    sticker frontend

---
+++ src/Controller/StickerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\OzonTaskFormType;
+use App\Form\OzonTaskProductFormType;
+use App\Model\DataObject\VariantProduct;
+use App\Utils\Utility;
+use Exception;
+use Pimcore\Controller\FrontendController;
+use Pimcore\Db;
+use Pimcore\Model\DataObject\Data\ObjectMetadata;
+use Pimcore\Model\DataObject\ListingTemplate;
+use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\Element\DuplicateFullPathException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Pimcore\Model\DataObject\Marketplace;
+
+
+class StickerController extends  FrontendController
+{
+    public function stickerMainPage(Request $request): Response
+    {
+
+        return $this->render('sticker/base.html.twig');
+    }
+}
