225860c1d6a5720fd22688d13a27f93da97101b7
enesbt <burdaenesvar@gmail.com>
2024-12-30 08:52:54 +0300

    find variant field bug

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -206,7 +206,9 @@ class PrepareOrderTableCommand extends AbstractCommand
         }
         $jsonPath = '$.' . $field;
         echo  "Json Path: $jsonPath\n";
-        Utility::fetchFromSqlFile($this->variantSqlfilePath . 'findVariant.sql', ['jsonPath' => $jsonPath, 'uniqueId' => $uniqueMarketplaceId]);
+        $result = Utility::fetchFromSqlFile($this->variantSqlfilePath . 'findVariant.sql', ['jsonPath' => $jsonPath, 'uniqueId' => $uniqueMarketplaceId]);
+        echo "Sql Result: ";
+        print_r($result);
         $objectId = $result[0]['object_id'] ?? null;
         if ($objectId) {
            return VariantProduct::getById($objectId);
