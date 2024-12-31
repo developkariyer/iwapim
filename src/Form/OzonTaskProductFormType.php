3b69e61f2c5e4e23c7dc9c3df0e7c21da1ad392a
Umut Demirhan <develop@kariyerfora.com>
2024-12-31 01:42:32 +0100

    ozon autolisting

--- src/Form/OzonTaskProductFormType.php
+++ src/Form/OzonTaskProductFormType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -63,7 +62,7 @@ class OzonTaskProductFormType extends AbstractType
             }
         }
 
-        $builder->add('productType', TextType::class, [
+        $builder->add('productType', ChoiceType::class, [
             'choices' => [],
             'label' => 'Ürün Tipi',
             'attr' => [
