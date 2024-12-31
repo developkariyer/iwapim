e1efc2ac1a2387d38d468decb64e0fdd9799d747
enesbt <burdaenesvar@gmail.com>
2024-12-31 11:49:16 +0300

    sticker frontend

--- src/SQL/Sticker/insert_into_sticker.sql
+++ src/SQL/Sticker/insert_into_sticker.sql
@@ -1,4 +1,4 @@
-INSERT INTO iwa_stickers (group_id, iwasku, product_code, category, product_name, attributes ,image_link, sticker_link)
+INSERT INTO iwa_stickers (group_id, iwasku, product_code, category, product_name, product_attributes ,image_link, sticker_link)
 VALUES (:group_id, :iwasku, :product_code, :category, :product_name,  :attributes, :image_link, :sticker_link);
 ON DUPLICATE KEY UPDATE
      group_id = VALUES(group_id),
