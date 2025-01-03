5a586b0624e1d37754bd14b0c43c13a0f017c505
Umut Demirhan <develop@kariyerfora.com>
2025-01-03 07:51:47 +0100

    ozon autolisting

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -2,7 +2,10 @@
 
 namespace App\Command;
 
+use Doctrine\DBAL\Exception;
 use Pimcore\Console\AbstractCommand;
+use Pimcore\Db;
+use Pimcore\Model\Asset;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -15,13 +18,28 @@ use Pimcore\Model\DataObject\GroupProduct;
 )]
 class HelloWorldCommand extends AbstractCommand
 {
+    /**
+     * @throws Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $db = Db::get();
         $gproduct = new GroupProduct\Listing();
         $result = $gproduct->load();
         foreach ($result as $item) {
-            $products = $item->getProducts();
-            print_r($products);
+            $products = $db->fetchAllAssociative("SELECT dest_id FROM object_relations_gproduct WHERE src_id = ? AND fieldname = 'products'", [$item->getId()]);
+            foreach ($products as $product) {
+                $details = $db->fetchAllAssociative("SELECT * FROM object_store_product WHERE oo_id = ?", [$product['dest_id']]);
+                $stickerId = $db->fetchOne("SELECT dest_id FROM object_relations_product WHERE src_id = ? AND type='asset' AND fieldname='sticker4x6'", [$product['dest_id']]);
+                if (!$stickerId) {
+                    // Burada sticker oluşturan kod çağrulmalı ve gelen id alınmalı
+                    usleep(1);
+                    // $stickerId = ??????
+                }
+                $sticker = Asset::getById($stickerId);
+                echo $sticker->getFullPath();
+                return Command::SUCCESS; // aksi halde çok fazla nesne çekmeye çalışacak
+            }
         }
 
 
