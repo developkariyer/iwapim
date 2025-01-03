4b3ddbfac96ad0d6320f82e1dea237bbdafbef67
enesbt <burdaenesvar@gmail.com>
2025-01-03 09:10:04 +0300

    frontend

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -10,6 +10,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use App\Utils\Utility;
 use App\Utils\Registry;
+use Pimcore\Model\DataObject\GroupProduct;
+
 #[AsCommand(
     name: 'app:hello-world',
     description: 'Outputs Hello, World!'
@@ -18,8 +20,15 @@ class HelloWorldCommand extends AbstractCommand
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $gproduct = new GroupProduct\Listing();
+        $result = $gproduct->load();
+        foreach ($result as $item) {
+            print_r($result);
+        }
+
+
         // Output "Hello, World!" as green text
-        $this->writeInfo("Hello, World!", $output);
+       // $this->writeInfo("Hello, World!", $output);
 
         // Return success status code
         return Command::SUCCESS;
