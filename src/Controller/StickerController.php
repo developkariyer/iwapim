ca5e552b07ffcb0c3b2552eb6a70ef1323a30208
enesbt <burdaenesvar@gmail.com>
2025-01-06 09:30:45 +0300

    sticker frontend

--- src/Controller/StickerController.php
+++ src/Controller/StickerController.php
@@ -94,7 +94,7 @@ class StickerController extends FrontendController
             FROM object_relations_gproduct org
                      JOIN object_product osp ON osp.oo_id = org.dest_id
                      LEFT JOIN object_relations_product opr ON opr.src_id = osp.oo_id AND opr.type = 'asset' AND opr.fieldname = 'sticker4x6eu'
-            WHERE org.src_id = :$groupId
+            WHERE org.src_id = $groupId
             LIMIT $limit OFFSET $offset;";
         $products = Db::get()->fetchAllAssociative($sql);
 
