d6649bb99160d471432aa0af0fca725b204eb6ee
enesbt <burdaenesvar@gmail.com>
2025-01-06 10:48:17 +0300

    sticker frontend

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -28,26 +28,6 @@ class HelloWorldCommand extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $limit = 10;
-        $offset = 0;
-        $groupId = 1;
-        $sql = "
-            SELECT
-                osp.iwasku,
-                osp.name AS product_name,
-                osp.productCode,
-                osp.productCategory,
-                osp.imageUrl,
-                osp.variationSize,
-                osp.variationColor,
-                opr.dest_id AS sticker_id
-            FROM object_relations_gproduct org
-                     JOIN object_product osp ON osp.oo_id = org.dest_id
-                     LEFT JOIN object_relations_product opr ON opr.src_id = osp.oo_id AND opr.type = 'asset' AND opr.fieldname = 'sticker4x6eu'
-            WHERE org.src_id = $groupId
-            LIMIT $limit OFFSET $offset;";
-        $products = Db::get()->fetchAllAssociative($sql);
-        print_r($products);
 
         // Output "Hello, World!" as green text
        // $this->writeInfo("Hello, World!", $output);
