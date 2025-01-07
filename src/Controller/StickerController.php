07fa4b78a5c52026eefa812770389e20e8551844
enesbt <burdaenesvar@gmail.com>
2025-01-07 10:00:19 +0300

    sticker frontend

--- src/Controller/StickerController.php
+++ src/Controller/StickerController.php
@@ -90,6 +90,7 @@ class StickerController extends FrontendController
             $searchCondition = "AND (iwasku LIKE :searchTerm OR name LIKE :searchTerm OR productCategory LIKE :searchTerm OR variationSize LIKE :searchTerm OR variationColor LIKE :searchTerm)";
             $offset = null;
         }
+
         $sql = "
         SELECT
             osp.iwasku,
@@ -104,10 +105,10 @@ class StickerController extends FrontendController
                  JOIN object_product osp ON osp.oo_id = org.dest_id
                  LEFT JOIN object_relations_product opr ON opr.src_id = osp.oo_id AND opr.type = 'asset' AND opr.fieldname = 'sticker4x6eu'
             WHERE org.src_id = :groupId
-            " . $searchCondition . ";
-        ";
+            " . $searchCondition . "
+            LIMIT $limit;";
         if ($offset !== null) {
-            $sql .= " LIMIT $limit OFFSET $offset";
+            $sql .= " OFFSET $offset";
         }
         $parameters = ['groupId' => (int) $groupId];
         if ($searchTerm) {
