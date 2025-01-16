d818595726584a375bc1b304ff1550002a2c090d
Umut Demirhan <develop@kariyerfora.com>
2025-01-17 00:21:08 +0100

    Frontend Controller Major Refactor

--- src/Controller/GoogleSheetsController.php
+++ src/Controller/GoogleSheetsController.php
@@ -12,6 +12,19 @@ class GoogleSheetsController extends FrontendController
 {
     static string $iwaskuSql = "SELECT DISTINCT iwasku FROM iwa_amazon_daily_sales_summary ORDER BY iwasku";
     static string $asin2iwaskuSql = "SELECT DISTINCT regkey AS asin, regvalue AS iwasku FROM iwa_registry WHERE regtype='asin-to-iwasku' ORDER BY asin";
+    static string $asinPreSql = "SELECT 
+    iwasku,
+    MAX(CASE WHEN sales_channel = 'Amazon.com' THEN asin END) AS us_asin,
+    MAX(CASE WHEN sales_channel = 'Amazon.eu' THEN asin END) AS eu_asin,
+    MAX(CASE WHEN sales_channel = 'Amazon.co.uk' THEN asin END) AS uk_asin,
+    MAX(CASE WHEN sales_channel = 'Amazon.ca' THEN asin END) AS ca_asin,
+    MAX(CASE WHEN sales_channel = 'Amazon.au' THEN asin END) AS au_asin,
+    MAX(CASE WHEN sales_channel = 'Amazon.co.jp' THEN asin END) AS jp_asin
+FROM 
+    iwa_amazon_daily_sales_summary
+GROUP BY 
+    iwasku;
+";
 
     /**
      * @Route("/sheets/main", name="sheets")
@@ -39,5 +52,15 @@ class GoogleSheetsController extends FrontendController
         return $this->json($iwaskuList);
     }
 
+    /**
+     * @Route("/sheets/amazonsales/pre", name="sheets_amazonsales_pre")
+     * @throws Exception
+     */
+    public function amazonSalesPreAction(): JsonResponse
+    {
+        $db = Db::get();
+        $saleData = $db->fetchAllAssociative(self::$asinPreSql);
+        return $this->json($saleData);
+    }
 
 }
