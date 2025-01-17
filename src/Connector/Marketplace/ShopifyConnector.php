ad9031f6ad5a161dd3e6488d68ad0d3a3195d8ea
enesbt <burdaenesvar@gmail.com>
2025-01-17 08:20:26 +0300

    shopify graphql

--- src/Connector/Marketplace/ShopifyConnector.php
+++ src/Connector/Marketplace/ShopifyConnector.php
@@ -361,8 +361,8 @@ class ShopifyConnector extends MarketplaceConnectorAbstract
     public function download($forceDownload = false): void
     {
         //$this->graphqlDownload();
-       //$this->downloadOrdersGraphql();
-       if (!$forceDownload && $this->getListingsFromCache()) {
+       $this->downloadOrdersGraphql();
+       /*if (!$forceDownload && $this->getListingsFromCache()) {
             echo "Using cached listings\n";
             return;
        }
@@ -371,7 +371,7 @@ class ShopifyConnector extends MarketplaceConnectorAbstract
             echo "Failed to download listings\n";
             return;
        }
-       $this->putListingsToCache();
+       $this->putListingsToCache();*/
     }
 
     /**
