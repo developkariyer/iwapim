a76b41fdbafaf95e2a09cff6d1cf957175e47da0
Umut Demirhan <develop@kariyerfora.com>
2025-01-20 10:58:15 +0100

    EAN automated controller

--- src/Controller/ShopifyController.php
+++ src/Controller/ShopifyController.php
@@ -26,40 +26,66 @@ class ShopifyController extends FrontendController
 
         $db = Db::get();
 
+        $variantProducts = $db->fetchAllAssociative("SELECT
+    osv.id,
+    osv.imageUrl,
+    TRIM(BOTH '\"' FROM SUBSTRING(
+        urlLink,
+        LOCATE('https://', urlLink),
+        LOCATE('\"', urlLink, LOCATE('https://', urlLink)) - LOCATE('https://', urlLink)
+    )) AS extractedUrl,
+    osv.title,
+    osv.uniqueMarketplaceId,
+    osv.salePrice,
+    osv.saleCurrency,
+    osv.quantity,
+    osv.lastUpdate,
+    osv.`key`   
+FROM
+    object_store_varyantproduct osv
+JOIN
+    object_relations_varyantproduct orvp
+    ON osv.oo_id = orvp.src_id
+    AND orvp.fieldname = 'marketplace'
+    AND orvp.dest_id = ?;", [$marketplaceId]);
 
 
-        $variantProductIds = $db->fetchAllAssociative("SELECT 
+
+        if (empty($variantProducts)) {
+            return new JsonResponse(['error' => 'No variant products found'], 404);
+        }
+
+        return new JsonResponse($variantProducts, 200);
+
+    }
+
+}
+
+
+/*
+        $variantProductIds = $db->fetchAllAssociative("SELECT
     osv.oo_id,
     osv.imageUrl,
  TRIM(BOTH '\"' FROM SUBSTRING(
         urlLink,
         LOCATE('https://', urlLink),
-        LOCATE('\"', urlLink, LOCATE('https://', urlLink)) - LOCATE('https://', urlLink) 
+        LOCATE('\"', urlLink, LOCATE('https://', urlLink)) - LOCATE('https://', urlLink)
     )) AS extractedUrl,
     op.iwasku,
     op.productCategory,
     op.key
-FROM 
+FROM
     object_store_varyantproduct osv
-JOIN 
+JOIN
     object_relations_varyantproduct orvp
     ON osv.oo_id = orvp.src_id
     AND orvp.fieldname = 'marketplace'
     AND orvp.dest_id = ?
-LEFT JOIN 
+LEFT JOIN
     object_relations_product orp
     ON orvp.src_id = orp.dest_id
     AND orp.fieldname = 'listingItems'
-LEFT JOIN 
+LEFT JOIN
     object_product op
     ON orp.src_id = op.oo_id;", [$marketplaceId]);
-
-        if (empty($variantProductIds)) {
-            return new JsonResponse(['error' => 'No variant products found'], 404);
-        }
-
-        return new JsonResponse($variantProductIds, 200);
-
-    }
-
-}
+*/
