d7f5e3a3aa6acbeac56f5e3eb51175fde3008ee4
Umut Demirhan <develop@kariyerfora.com>
2025-01-22 00:05:17 +0100

    EAN automated controller

---
+++ src/Command/RemoveNumberedFoldersCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Command;
+
+use App\Utils\Utility;
+use Pimcore\Console\AbstractCommand;
+use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\Element\DuplicateFullPathException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:remove-nums',
+    description: 'Remove numbered folders!'
+)]
+class RemoveNumberedFoldersCommand extends AbstractCommand
+{
+    private string $sqlPath = PIMCORE_PROJECT_ROOT . '/src/SQL/Sticker/';
+    /**
+     * @throws DuplicateFullPathException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $urunler = Utility::checkSetPath('Ürünler');
+        $productListingObject = new Product\Listing();
+        $products = $productListingObject->load();
+        $index = 0;
+        foreach ($products as $product) {
+            $index++;
+            echo $index . ' / ' . count($products) ."      \r";
+            if ($product->level()==1) {
+                continue;
+            }
+            $parent = $product->getParent();
+            $grandParent = $parent->getParent();
+            if ($grandParent === $urunler) {
+                echo $product->getFullPath() . PHP_EOL;
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+}
