4d95076c6bf8c14082dbf039f121275527be816d
enesbt <burdaenesvar@gmail.com>
2025-01-27 17:08:46 +0300

    shopify graphql

--- src/Connector/Marketplace/ShopifyConnector.php
+++ src/Connector/Marketplace/ShopifyConnector.php
@@ -552,35 +552,6 @@ class ShopifyConnector extends MarketplaceConnectorAbstract
         }
     }
 
-    // Not Used
-    private function downloadAbondonedCheckouts(): void
-    {
-        echo "Downloading abandoned checkouts\n";
-        $db = Db::get();
-        $lastUpdatedAt = $db->fetchOne(
-            "SELECT COALESCE(MAX(json_extract(json, '$.updated_at')), '2000-01-01T00:00:00Z') FROM iwa_marketplace_abandoned_checkouts WHERE marketplace_id = ?",
-            [$this->marketplace->getId()]
-        );
-        $checkouts = $this->getFromShopifyApi('GET', 'checkouts.json', ['status' => 'any', 'updated_at_min' => $lastUpdatedAt], 'checkouts');
-        $db->beginTransaction();
-        try {
-            foreach ($checkouts as $checkout) {
-                $db->executeStatement(
-                    "INSERT INTO iwa_marketplace_abandoned_checkouts (marketplace_id, checkout_id, json) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE json = VALUES(json)",
-                    [
-                        $this->marketplace->getId(),
-                        $checkout['id'],
-                        json_encode($checkout)
-                    ]
-                );
-            }
-            $db->commit();
-        } catch (\Exception $e) {
-            $db->rollBack();
-            echo "Error: " . $e->getMessage() . "\n";
-        }
-    }
-
     /**
      * @throws DuplicateFullPathException
      */
@@ -591,7 +562,7 @@ class ShopifyConnector extends MarketplaceConnectorAbstract
         }
         $marketplaceFolder = Utility::checkSetPath(
             Utility::sanitizeVariable($this->marketplace->getKey(), 190),
-            Utility::checkSetPath('Pazaryerleri/Test')
+            Utility::checkSetPath('Pazaryerleri')
         );
         $total = count($this->listings);
         $index = 0;
