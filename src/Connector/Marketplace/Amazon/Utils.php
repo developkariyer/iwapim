0089374a9340b108e21fbccca697f9641164acd4
Umut Demirhan <develop@kariyerfora.com>
2025-01-29 18:36:05 +0100

    UI notification for commands

--- src/Connector/Marketplace/Amazon/Utils.php
+++ src/Connector/Marketplace/Amazon/Utils.php
@@ -197,12 +197,12 @@ class Utils
             new PatchOperation(
                 op: "replace",
                 path: "/attributes/externally_assigned_product_identifier",
-                value: [
+                value: $ean /*[
                     [
                         "marketplace_id" => AmazonConstants::amazonMerchant[$country]['id'],
                         "value" => $ean
                     ]
-                ]
+                ]*/
             )
         ];
         $this->patchCustom($sku, $country, $patches);
