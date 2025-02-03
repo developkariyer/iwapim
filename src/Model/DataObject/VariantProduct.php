6d309edbf3d16edd547a8a21fdb438f0e077fd98
Umut Demirhan <develop@kariyerfora.com>
2025-02-03 20:43:18 +0100

    UI notification for commands

--- src/Model/DataObject/VariantProduct.php
+++ src/Model/DataObject/VariantProduct.php
@@ -85,7 +85,6 @@ class VariantProduct extends Concrete
                     $product = Product::getByIwasku($variant['sku'], 1);
                     if ($product instanceof Product) {
                         echo "    - Found product: {$product->getId()}\n";
-                        sleep(3);
                     } else {
                         echo "    - Product not found.\n";
                     }
