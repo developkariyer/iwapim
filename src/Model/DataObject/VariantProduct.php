7320eae214b6be5de479a7bd0bf3bab1a66d90c6
enesbt <burdaenesvar@gmail.com>
2025-02-05 08:45:42 +0300

    shopify graphql

--- src/Model/DataObject/VariantProduct.php
+++ src/Model/DataObject/VariantProduct.php
@@ -151,7 +151,7 @@ class VariantProduct extends Concrete
         $this->setSaleCurrency($variant['saleCurrency'] ?? '');
         $this->setTitle($variant['title'] ?? '');
         $this->setAttributes($variant['attributes'] ?? '');
-        $this->setEan('');
+        //$this->setEan($variant['ean']  '');
         $this->setQuantity($variant['quantity'] ?? 0);
         $this->setUniqueMarketplaceId($variant['uniqueMarketplaceId'] ?? '');
         $this->setMarketplace($marketplace);
