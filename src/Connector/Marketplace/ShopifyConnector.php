fe0ad7683e0d910efe85218291f211b84a7e2e58
enesbt <burdaenesvar@gmail.com>
2025-02-06 11:19:53 +0300

    shopify graphql

--- src/Connector/Marketplace/ShopifyConnector.php
+++ src/Connector/Marketplace/ShopifyConnector.php
@@ -426,7 +426,7 @@ class ShopifyConnector extends MarketplaceConnectorAbstract
         $this->listings = $this->getFromCache("LISTINGS.json");
 
         $marketplaceFolder = Utility::checkSetPath(
-            Utility::sanitizeVariable('Test9' . $this->marketplace->getKey(), 190),
+            Utility::sanitizeVariable('Test10' . $this->marketplace->getKey(), 190),
             Utility::checkSetPath('Pazaryerleri')
         );
         $total = count($this->listings);
