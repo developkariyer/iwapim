a15f2631efce0063f6edf452f8c8b49f2ce5c7a2
enesbt <burdaenesvar@gmail.com>
2025-02-11 16:47:02 +0300

    ebay connector

--- src/Connector/Marketplace/EbayConnector.php
+++ src/Connector/Marketplace/EbayConnector.php
@@ -113,7 +113,7 @@ class EbayConnector extends MarketplaceConnectorAbstract
                  <StartTimeTo>2025-02-11T00:00:00.000Z</StartTimeTo>
               <IncludeWatchCount>true</IncludeWatchCount>
               <IncludeVariations>true</IncludeVariations>
-              <SKUArray>true</SKUArray>
+              <SKUArray>50</SKUArray>
               <Pagination>
                 <EntriesPerPage>2</EntriesPerPage>
               </Pagination>
