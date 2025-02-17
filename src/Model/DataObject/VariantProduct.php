83e4f53a6270970074e3c8889772ff9f4b5882d9
enesbt <burdaenesvar@gmail.com>
2025-02-17 11:28:03 +0300

    ebay connector

--- src/Model/DataObject/VariantProduct.php
+++ src/Model/DataObject/VariantProduct.php
@@ -84,9 +84,9 @@ class VariantProduct extends Concrete
                         $product = Product::getByIwasku($variant['sku'], 1);
                     }
                 }
-               /* if (empty($product) && !empty($variant['ean'])) {
+                if (empty($product) && !empty($variant['ean'])) {
                     $product = Product::getByEanGtin($variant['ean'], 1);
-                }*/
+                }
                 if (isset($product) && $product instanceof Product) {
                     echo "C";
                     $product->addVariant($object);
@@ -149,7 +149,7 @@ class VariantProduct extends Concrete
         $this->setSaleCurrency($variant['saleCurrency'] ?? '');
         $this->setTitle($variant['title'] ?? '');
         $this->setAttributes($variant['attributes'] ?? '');
-        //$this->setEan($variant['ean'] ?? '');
+        $this->setEan($variant['ean'] ?? '');
         $this->setQuantity($variant['quantity'] ?? 0);
         $this->setUniqueMarketplaceId($variant['uniqueMarketplaceId'] ?? '');
         $this->setMarketplace($marketplace);
