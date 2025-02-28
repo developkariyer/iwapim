41199d57bb442dc5774120d3450eff6e221bb96f
enesbt <burdaenesvar@gmail.com>
2025-02-28 14:01:31 +0300

    ebay

--- src/Connector/Marketplace/EbayConnector.php
+++ src/Connector/Marketplace/EbayConnector.php
@@ -126,7 +126,7 @@ class EbayConnector extends MarketplaceConnectorAbstract
 
     public function getItemRest($itemId)
     {
-        $url = "https://api.ebay.com/buy/browse/v1/item/" . $itemId;
+        $url = "https://api.ebay.com/item/" . $itemId;
         try {
             $response = $this->httpClient->request('GET', $url, [
                 'headers' => [
@@ -176,8 +176,8 @@ class EbayConnector extends MarketplaceConnectorAbstract
     public function download(bool $forceDownload = false): void
     {
        // $this->refreshToAccessToken();
-        //$this->getItemRest("334936877779");
-        $this->listingDetail("334936877779");
+        $this->getItemRest("334936877779");
+        //$this->listingDetail("334936877779");
         //$this->getMyeBaySelling();
         //$this->getItemByLegacyId("334936898383");
 
