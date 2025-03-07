f89a6d416ff9fb36e4cd8e728c96032c31b26b9c
enesbt <burdaenesvar@gmail.com>
2025-03-07 15:59:14 +0300

    hepsiburada order

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -148,7 +148,7 @@ class PrepareOrderTableCommand extends AbstractCommand
     protected function prepareOrderTable($uniqueMarketplaceId, $marketplaceType): void
     {
         $variantObject = match ($marketplaceType) {
-            'Shopify', 'Etsy', 'Amazon', 'Takealot', 'Ciceksepeti' => $this->findVariantProduct($uniqueMarketplaceId),
+            'Shopify', 'Etsy', 'Amazon', 'Takealot', 'Ciceksepeti', 'Hepsiburada' => $this->findVariantProduct($uniqueMarketplaceId),
             'Trendyol' => $this->findVariantProduct($uniqueMarketplaceId,'"productCode"'),
             'Bol.com' => $this->findVariantProduct($uniqueMarketplaceId,'"product-ids".bolProductId'),
             'Wallmart' => $this->findVariantProduct($uniqueMarketplaceId,'"sku"'),
