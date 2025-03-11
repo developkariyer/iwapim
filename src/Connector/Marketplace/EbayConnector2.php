b86b68f48f4397d5dce7e030b7da74e4e07d8a19
Umut Demirhan <develop@kariyerfora.com>
2025-03-12 00:58:10 +0100

    ebay connector refactor

--- src/Connector/Marketplace/EbayConnector2.php
+++ src/Connector/Marketplace/EbayConnector2.php
@@ -18,25 +18,25 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 class EbayConnector2 extends MarketplaceConnectorAbstract
 {
     private static string $scopes = "https://api.ebay.com/oauth/api_scope
-        https://api.ebay.com/oauth/api_scope/sell.marketing.readonly
-        https://api.ebay.com/oauth/api_scope/sell.marketing
-        https://api.ebay.com/oauth/api_scope/sell.inventory.readonly
-        https://api.ebay.com/oauth/api_scope/sell.inventory
-        https://api.ebay.com/oauth/api_scope/sell.account.readonly
-        https://api.ebay.com/oauth/api_scope/sell.account
-        https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly
-        https://api.ebay.com/oauth/api_scope/sell.fulfillment
-        https://api.ebay.com/oauth/api_scope/sell.analytics.readonly
-        https://api.ebay.com/oauth/api_scope/sell.finances
-        https://api.ebay.com/oauth/api_scope/sell.payment.dispute
-        https://api.ebay.com/oauth/api_scope/commerce.identity.readonly
-        https://api.ebay.com/oauth/api_scope/sell.reputation 
-        https://api.ebay.com/oauth/api_scope/sell.reputation.readonly 
-        https://api.ebay.com/oauth/api_scope/commerce.notification.subscription 
-        https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly 
-        https://api.ebay.com/oauth/api_scope/sell.stores 
-        https://api.ebay.com/oauth/api_scope/sell.stores.readonly 
-        https://api.ebay.com/oauth/scope/sell.edelivery";
+https://api.ebay.com/oauth/api_scope/sell.marketing.readonly
+https://api.ebay.com/oauth/api_scope/sell.marketing
+https://api.ebay.com/oauth/api_scope/sell.inventory.readonly
+https://api.ebay.com/oauth/api_scope/sell.inventory
+https://api.ebay.com/oauth/api_scope/sell.account.readonly
+https://api.ebay.com/oauth/api_scope/sell.account
+https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly
+https://api.ebay.com/oauth/api_scope/sell.fulfillment
+https://api.ebay.com/oauth/api_scope/sell.analytics.readonly
+https://api.ebay.com/oauth/api_scope/sell.finances
+https://api.ebay.com/oauth/api_scope/sell.payment.dispute
+https://api.ebay.com/oauth/api_scope/commerce.identity.readonly
+https://api.ebay.com/oauth/api_scope/sell.reputation 
+https://api.ebay.com/oauth/api_scope/sell.reputation.readonly 
+https://api.ebay.com/oauth/api_scope/commerce.notification.subscription 
+https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly 
+https://api.ebay.com/oauth/api_scope/sell.stores 
+https://api.ebay.com/oauth/api_scope/sell.stores.readonly 
+https://api.ebay.com/oauth/scope/sell.edelivery";
 
 
     private string $accessToken = '';
