c1da01e33797ae3faa6c2626cf28adf31631e762
enesbt <burdaenesvar@gmail.com>
2025-03-21 10:50:16 +0300

    orders update

--- src/SQL/OrderTable/Transfer/iwa_marketplace_orders_transfer_ebay.sql
+++ src/SQL/OrderTable/Transfer/iwa_marketplace_orders_transfer_ebay.sql
@@ -1,7 +1,7 @@
 INSERT INTO iwa_marketplace_orders_line_items (
     marketplace_type, marketplace_id, created_at, order_id, product_id, variant_id, price, currency, quantity, variant_title,
     shipping_province, shipping_city, shipping_country_code, total_price, subtotal_price,
-    fulfillments_status, fulfillments_status_control
+    fulfillments_status, fulfillments_status_control, customer_first_name, customer_last_name
 )
 SELECT
     :marketplaceType,
@@ -20,28 +20,33 @@ SELECT
     COALESCE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.pricingSummary.total.value')), NULL) AS total_price,
     COALESCE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.pricingSummary.priceSubtotal.value')), NULL) AS subtotal_price,
     COALESCE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.orderFulfillmentStatus')), NULL) AS fulfillments_status,
-    COALESCE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.cancelStatus.cancelState')), NULL) AS fulfillments_status_control
+    COALESCE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.cancelStatus.cancelState')), NULL) AS fulfillments_status_control,
+    SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT(json, '$.buyer.buyerRegistrationAddress.fullName')), ' ', LENGTH(JSON_UNQUOTE(JSON_EXTRACT(json, '$.buyer.buyerRegistrationAddress.fullName'))) -
+    LENGTH(REPLACE(JSON_UNQUOTE(JSON_EXTRACT(json, '$.buyer.buyerRegistrationAddress.fullName')), ' ', ''))) AS customer_first_name,
+    SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT(json, '$.buyer.buyerRegistrationAddress.fullName')), ' ', -1) AS customer_last_name
 FROM
     iwa_marketplace_orders
     CROSS JOIN JSON_TABLE(json, '$.lineItems[*]' COLUMNS ( value JSON PATH '$' )) AS line_item
 WHERE
-     marketplace_id = :marketPlaceId
+    marketplace_id = :marketPlaceId
 ON DUPLICATE KEY UPDATE
-     marketplace_type = VALUES(marketplace_type),
-     marketplace_id = VALUES(marketplace_id),
-     created_at = VALUES(created_at),
-     closed_at = VALUES(closed_at),
-     product_id = VALUES(product_id),
-     variant_id = VALUES(variant_id),
-     price = VALUES(price),
-     currency = VALUES(currency),
-     quantity = VALUES(quantity),
-     variant_title = VALUES(variant_title),
-     shipping_province = VALUES(shipping_province),
-     shipping_city = VALUES(shipping_city),
-     shipping_company = VALUES(shipping_company),
-     shipping_country_code = VALUES(shipping_country_code),
-     total_price = VALUES(total_price),
-     subtotal_price = VALUES(subtotal_price),
-     fulfillments_status = VALUES(fulfillments_status),
-     fulfillments_status_control = VALUES(fulfillments_status_control)
+    marketplace_type = VALUES(marketplace_type),
+    marketplace_id = VALUES(marketplace_id),
+    created_at = VALUES(created_at),
+    closed_at = VALUES(closed_at),
+    product_id = VALUES(product_id),
+    variant_id = VALUES(variant_id),
+    price = VALUES(price),
+    currency = VALUES(currency),
+    quantity = VALUES(quantity),
+    variant_title = VALUES(variant_title),
+    shipping_province = VALUES(shipping_province),
+    shipping_city = VALUES(shipping_city),
+    shipping_company = VALUES(shipping_company),
+    shipping_country_code = VALUES(shipping_country_code),
+    total_price = VALUES(total_price),
+    subtotal_price = VALUES(subtotal_price),
+    fulfillments_status = VALUES(fulfillments_status),
+    fulfillments_status_control = VALUES(fulfillments_status_control),
+    customer_first_name = VALUES(customer_first_name),
+    customer_last_name = VALUES(customer_last_name)
