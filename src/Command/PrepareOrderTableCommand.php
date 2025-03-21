71c0a52cbce0484b5a09c9c9cec99f9b93e2ea10
enesbt <burdaenesvar@gmail.com>
2025-03-21 10:51:32 +0300

    orders update

--- src/Command/PrepareOrderTableCommand.php
+++ src/Command/PrepareOrderTableCommand.php
@@ -93,7 +93,8 @@ class PrepareOrderTableCommand extends AbstractCommand
             'Wallmart' => 'iwa_marketplace_orders_transfer_wallmart.sql',
             'Ciceksepeti' => 'iwa_marketplace_orders_transfer_ciceksepeti.sql',
             'Wayfair' => 'iwa_marketplace_orders_transfer_wayfair.sql',
-            'Hepsiburada' => 'iwa_marketplace_orders_transfer_hepsiburada.sql'
+            'Hepsiburada' => 'iwa_marketplace_orders_transfer_hepsiburada.sql',
+            'Ebay' => 'iwa_marketplace_orders_transfer_ebay.sql'
         ];
         foreach ($marketplaceIds as $marketplaceId) {
             $id = $marketplaceId['marketplace_id'];
