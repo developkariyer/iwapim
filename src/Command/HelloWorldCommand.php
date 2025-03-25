2de85239a6abecdd1fcd14ffd59a24f53ecf072b
enesbt <burdaenesvar@gmail.com>
2025-03-25 14:34:02 +0300

    test

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -28,7 +28,7 @@ class HelloWorldCommand extends AbstractCommand
         $product = Product::findByField('iwasku', 'ITS00402Z5XC');
         $product->checkStickerFnsku();
         $group = GroupProduct::getById(271263);
-        $group->setProducts($product);
+        $group->setProducts([$product]);
        /* if ($product instanceof Product) {
             echo "Finded\n" ;
             $variantProducts =  $product->getListingItems();
