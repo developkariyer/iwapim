bdff4362617b42a68c36a165a8b8d85fb66207cd
enesbt <burdaenesvar@gmail.com>
2025-04-09 08:51:52 +0300

    ebay test

--- src/Connector/Marketplace/EbayConnector.php
+++ src/Connector/Marketplace/EbayConnector.php
@@ -174,7 +174,7 @@ class EbayConnector extends MarketplaceConnectorAbstract
      */
     public function download(bool $forceDownload = false): void
     {
-        $this->getItemRest("335124005550");
+        $this->getItemRest("334921595917");
         //$this->listingDetail("334936877779");
         //$this->getMyeBaySelling();
         //$this->getItemByLegacyId("334936877779");
