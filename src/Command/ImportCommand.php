15d37ac6372b3e06148090b9b7d06442e0c3d0ad
enesbt <burdaenesvar@gmail.com>
2025-04-10 16:39:55 +0300

    connector

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -43,12 +43,10 @@ class ImportCommand extends AbstractCommand
     private DataObjectListener $dataObjectListener;
     private NotificationService $notificationService;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher, DataObjectListener $dataObjectListener, NotificationService  $notificationService)
+    public function __construct(EventDispatcherInterface $eventDispatcher)
     {
         parent::__construct();
         $this->eventDispatcher = $eventDispatcher;
-        $this->dataObjectListener = $dataObjectListener;
-        $this->notificationService = $notificationService;
     }
 
     protected function configure(): void
@@ -82,7 +80,7 @@ class ImportCommand extends AbstractCommand
         ;
     }
 
-    private function removeListeners(): void
+    /*private function removeListeners(): void
     {
         $this->eventDispatcher->removeSubscriber($this->dataObjectListener);
     }
@@ -90,7 +88,7 @@ class ImportCommand extends AbstractCommand
     private function addListeners(): void
     {
         $this->eventDispatcher->addSubscriber($this->dataObjectListener);
-    }
+    }*/
 
     private static function listMarketplaces($marketplaces): int
     {
@@ -144,7 +142,7 @@ class ImportCommand extends AbstractCommand
         $ozonFlag = $input->getOption('ozon');
         $allFlag = $input->getOption('all');
 
-        $this->removeListeners();
+        //$this->removeListeners();
 
         $notificationMessage = "Import report for ".date("Y-m-d H:i:s").":\n";
         $notificationSendFlag = false;
@@ -264,14 +262,14 @@ class ImportCommand extends AbstractCommand
                 echo "done.\n";
                 $notificationMessage .= "\n";
             }
-            if ($notificationSendFlag) {
+            /*if ($notificationSendFlag) {
                 $this->notificationService->sendToUser(2, 1, 'Import completed!', $notificationMessage);
-            }
-            $this->addListeners();
+            }*/
+            //$this->addListeners();
             return Command::SUCCESS;
         } catch (Exception|\Exception) {
-            $this->notificationService->sendToUser(2, 1, 'Import failed!', "An error occurred while importing listings. Here is where it stopped:\n$notificationMessage");
-            $this->addListeners();
+            //$this->notificationService->sendToUser(2, 1, 'Import failed!', "An error occurred while importing listings. Here is where it stopped:\n$notificationMessage");
+            //$this->addListeners();
             return Command::FAILURE;
         }
     }
