fb9371cb0ee422b15efa55a6ed674cb1a09b430a
enesbt <burdaenesvar@gmail.com>
2025-04-15 16:17:10 +0300

    ciceksepeti connector

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -228,7 +228,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
     public function downloadInventory(): void
     {
         //$this->downloadCategories();
-        $this->getCategoryAttributesAndSaveDatabase(13077);
+        $this->getCategoryAttributesAndSaveDatabase(12691);
     }
 
     /**
