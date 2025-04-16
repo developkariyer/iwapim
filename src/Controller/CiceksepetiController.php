38c0432f1e02cb06e6a5d3074e270f7321bf68c9
enesbt <burdaenesvar@gmail.com>
2025-04-16 13:43:06 +0300

    ciceksepeti connector

--- src/Controller/CiceksepetiController.php
+++ src/Controller/CiceksepetiController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Model\DataObject\VariantProduct;
+use App\Utils\Utility;
 use Doctrine\DBAL\Exception;
 use Pimcore\Db;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,7 +23,44 @@ class CiceksepetiController extends FrontendController
      */
     public function ciceksepetiMainPage(): Response
     {
-
-        return $this->render('ciceksepeti/ciceksepeti.html.twig');
+        return $this->render('ciceksepeti/ciceksepeti.html.twig', [
+            'ciceksepetiListings' => $this->getCiceksepetiListings()
+        ]);
     }
+
+    public function getCiceksepetiListings(): array
+    {
+        $sql = "SELECT oo_id FROM `object_query_varyantproduct` WHERE marketplaceType = 'Ciceksepeti'";
+        $ciceksepetiVariantIds = Utility::fetchFromSql($sql);
+        $ciceksepetiVariant = [];
+        foreach ($ciceksepetiVariantIds as $ciceksepetiVariantId) {
+            $variantProduct = VariantProduct::getById($ciceksepetiVariantId['oo_id']);
+            if (!$variantProduct instanceof VariantProduct) {
+                continue;
+            }
+            $apiData = json_decode($variantProduct->jsonRead('apiResponseJson'), true);
+            $ciceksepetiVariant[] = [
+                'link' => $apiData['link'],
+                'images' => $apiData['images'],
+                'barcode' => $apiData['barcode'],
+                'variantIsActive' => $apiData['isActive'],
+                'listPrice' => $apiData['listPrice'],
+                'stockCode' => $apiData['stockCode'],
+                'attributes' => $apiData['attributes'],
+                'salesPrice' => $apiData['salesPrice'],
+                'description' => $apiData['description'],
+                'productCode' => $apiData['productCode'],
+                'productName' => $apiData['productName'],
+                'deliveryType' => $apiData['deliveryType'],
+                'stockQuantity' => $apiData['stockQuantity'],
+                'commissionRate' => $apiData['commissionRate'],
+                'mainProductCode' => $apiData['mainProductCode'],
+                'numberOfFavorites' => $apiData['numberOfFavorites'],
+                'productIsActive' => $apiData['productStatusType'],
+                'deliveryMessageType' => $apiData['deliveryMessageType'],
+            ];
+        }
+        return $ciceksepetiVariant;
+    }
+
 }
