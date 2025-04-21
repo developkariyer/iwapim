e0d054377fc533e94ece8e7fc4d54e0ae7620911
enesbt <burdaenesvar@gmail.com>
2025-04-21 14:41:30 +0300

    auth test

--- src/Controller/CiceksepetiController.php
+++ src/Controller/CiceksepetiController.php
@@ -18,7 +18,7 @@ use Pimcore\Model\DataObject\Data\Link;
 use Pimcore\Model\Asset;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_PIMCORE_ADMIN')]
+#[IsGranted('ROLE_STICKERMANAGER')]
 class CiceksepetiController extends FrontendController
 {
     /**
