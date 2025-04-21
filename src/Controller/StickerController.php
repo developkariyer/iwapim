2389a88cbe4053de182cb3eee58e0e87eb452e43
enesbt <burdaenesvar@gmail.com>
2025-04-21 15:01:00 +0300

    auth test

--- src/Controller/StickerController.php
+++ src/Controller/StickerController.php
@@ -16,8 +16,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\ExpressionLanguage\Expression;
 
-#[IsGranted('ROLE_STICKERMANAGER || ROLE_PIMCORE_ADMIN')]
+#[IsGranted(new Expression("is_granted('ROLE_STICKERMANAGER') or is_granted('ROLE_PIMCORE_ADMIN')"))]
 class StickerController extends FrontendController
 {
     protected function getGroupList(): array
