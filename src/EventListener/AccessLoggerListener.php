cbb2b77dae9937868389799c53fecc85d07783e8
enesbt <burdaenesvar@gmail.com>
2025-04-21 16:05:39 +0300

    auth test

---
+++ src/EventListener/AccessLoggerListener.php
@@ -0,0 +1,67 @@
+<?php
+// src/EventListener/AccessLoggerListener.php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Security;
+
+class AccessLoggerListener implements EventSubscriberInterface
+{
+    private LoggerInterface $logger;
+    private Security $security;
+
+    public function __construct(LoggerInterface $accessLogger, Security $security)
+    {
+        $this->logger = $accessLogger;
+        $this->security = $security;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onKernelController',
+        ];
+    }
+
+    public function onKernelController(ControllerEvent $event): void
+    {
+        $controllerCallable = $event->getController();
+        if (is_array($controllerCallable)) {
+            $controllerClass = get_class($controllerCallable[0]);
+            $controllerMethod = $controllerCallable[1];
+        } elseif (is_object($controllerCallable) && method_exists($controllerCallable, '__invoke')) {
+            $controllerClass = get_class($controllerCallable);
+            $controllerMethod = '__invoke';
+        } else {
+            $this->logger->warning('AccessLogger: Anlaşılamayan controller tipi.', ['controller' => $controllerCallable]);
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        $routeName = $request->attributes->get('_route');
+
+        $user = $this->security->getUser();
+
+        $logMessage = sprintf(
+            'Access Granted: Route "%s" (%s::%s)',
+            $routeName ?? 'N/A',
+            $controllerClass,
+            $controllerMethod
+        );
+        $logContext = [
+            'user_id' => $user ? $user->getId() : 'ANONYMOUS',
+            'username' => $user ? $user->getUserIdentifier() : 'ANONYMOUS',
+            'roles' => $user ? $user->getRoles() : ['IS_AUTHENTICATED_ANONYMOUSLY'],
+            'ip_address' => $request->getClientIp(),
+            'method' => $request->getMethod(),
+            'uri' => $request->getRequestUri(),
+        ];
+
+        $this->logger->info($logMessage, $logContext);
+    }
+}
