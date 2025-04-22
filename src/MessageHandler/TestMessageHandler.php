504c32cdfeb858172f76fc38e7b04046afe44e38
enesbt <burdaenesvar@gmail.com>
2025-04-22 10:35:11 +0300

    message queue test

--- src/MessageHandler/TestMessageHandler.php
+++ src/MessageHandler/TestMessageHandler.php
@@ -3,10 +3,9 @@ namespace App\MessageHandler;
 
 use App\Message\TestMessage;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 #[AsMessageHandler]
-class TestMessageHandler implements MessageHandlerInterface
+class TestMessageHandler
 {
     public function __invoke(TestMessage $message)
     {
