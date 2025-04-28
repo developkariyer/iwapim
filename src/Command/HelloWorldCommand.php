0a27bd468c58bb864e773a6d7f2bee1c8da12b65
enesbt <burdaenesvar@gmail.com>
2025-04-28 11:13:14 +0300

    message queue test

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -32,7 +32,8 @@ class HelloWorldCommand extends AbstractCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $sql = "SELECT 
+        $sql = "
+            SELECT 
             c.id AS category_id,
             c.category_name,
             JSON_ARRAYAGG(
@@ -42,33 +43,31 @@ class HelloWorldCommand extends AbstractCommand
                     'attribute_type', ca.type,
                     'is_required', ca.is_required,
                     'attribute_values', 
-                    (
-                        SELECT JSON_ARRAYAGG(
-                            JSON_OBJECT(
-                                'attribute_value_id', cav.attribute_value_id,
-                                'attribute_value', cav.name
-                            )
+                    JSON_ARRAYAGG(
+                        JSON_OBJECT(
+                            'attribute_value_id', cav.attribute_value_id,
+                            'attribute_value', cav.name
                         )
-                        FROM iwa_ciceksepeti_category_attributes_values cav
-                        WHERE cav.attribute_id = ca.attribute_id
                     )
                 )
             ) AS attributes_json
-        FROM 
-            iwa_ciceksepeti_categories c
-        JOIN 
-            iwa_ciceksepeti_category_attributes ca 
-        ON 
-            c.id = ca.category_id
-        WHERE 
-            ca.type IN ('Ürün Özelliği', 'Variant Özelliği')
-        AND 
-            c.id IN (16105, 14156)  
-        GROUP BY 
-            c.id, c.category_name;";
+            FROM 
+                iwa_ciceksepeti_categories c
+            JOIN 
+                iwa_ciceksepeti_category_attributes ca 
+                ON c.id = ca.category_id
+            JOIN 
+                iwa_ciceksepeti_category_attributes_values cav
+                ON ca.attribute_id = cav.attribute_id
+            WHERE 
+                ca.type IN ('Ürün Özelliği', 'Variant Özelliği')
+            AND 
+                c.id IN (16105, 14156)  -- Bu kategorilerin ID'lerini değiştirebilirsiniz
+            GROUP BY 
+                c.id, c.category_name;
+        ";
         $result = Utility::fetchFromSql($sql);
         print_r($result);
-
         /*$productId = 238133;
         $variantIds = [240430, 240431, 240433, 240434, 240436, 240437];
 
