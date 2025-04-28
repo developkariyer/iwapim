41e4522334563ffa529501d4a186c3d2f3574202
enesbt <burdaenesvar@gmail.com>
2025-04-28 16:09:04 +0300

    gemini prompt

--- src/MessageHandler/CiceksepetiListingHandler.php
+++ src/MessageHandler/CiceksepetiListingHandler.php
@@ -77,7 +77,7 @@ class CiceksepetiListingHandler
         EOD;
         $result = $this->getGeminiApi($promt);
         $text = $result['candidates'][0]['content']['parts'][0]['text'];
-        $text = str_replace(['```json', '```'], '', $text); 
+        $text = str_replace(['```json', '```'], '', $text);
         $data = json_decode($text, true);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
