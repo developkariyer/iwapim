99fb8cdd88399ee764b86a8c46305bafdea9b7ac
enesbt <burdaenesvar@gmail.com>
2025-05-07 16:35:51 +0300

    test page

--- src/Controller/AutoListingController.php
+++ src/Controller/AutoListingController.php
@@ -29,7 +29,9 @@ class AutoListingController extends FrontendController
      */
     public function autolistingMainPage(): Response
     {
-        return $this->render('autolisting/autolisting.html.twig');
+        return $this->render('autolisting/autolisting.html.twig', [
+            'ciceksepeticCategories' => $this->getCiceksepetiListingCategories(),
+        ]);
     }
 
     /**
@@ -154,6 +156,41 @@ class AutoListingController extends FrontendController
                 'message' => 'İşlem sırasında bir hata oluştu: ' . $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public function getCiceksepetiListingCategories()
+    {
+        $categoryIdList = $this->getCiceksepetiListingCategoriesIdList();
+        $categoryIdString = implode(',', array_map('intval', $categoryIdList));
+        $sqlCategory = "SELECT id, category_name FROM iwa_ciceksepeti_categories WHERE id IN ($categoryIdString)";
+        $categories = Utility::fetchFromSql($sqlCategory);
+        $result = [];
+        foreach ($categories as $cat) {
+            $result[] = [
+                'id' => $cat['id'],
+                'name' => $cat['category_name']
+            ];
+        }
+        return $result;
+    }
+
+    public function getCiceksepetiListingCategoriesIdList(): array
+    {
+        $sql = "SELECT oo_id FROM `object_query_varyantproduct` WHERE marketplaceType = 'Ciceksepeti'";
+        $ciceksepetiVariantIds = Utility::fetchFromSql($sql);
+        if (!is_array($ciceksepetiVariantIds) || empty($ciceksepetiVariantIds)) {
+            return [];
+        }
+        $categoryIdList = [];
+        foreach ($ciceksepetiVariantIds as $ciceksepetiVariantId) {
+            $variantProduct = VariantProduct::getById($ciceksepetiVariantId['oo_id']);
+            if (!$variantProduct instanceof VariantProduct) {
+                continue;
+            }
+            $apiData = json_decode($variantProduct->jsonRead('apiResponseJson'), true);
+            $categoryIdList[] = $apiData['categoryId'];
+        }
+        return array_unique($categoryIdList);
     }
 
 
