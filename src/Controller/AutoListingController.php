d2d60a1aacd0f2a0808d77f70c06136fa4ca6769
enesbt <burdaenesvar@gmail.com>
2025-05-07 14:42:54 +0300

    test page

---
+++ src/Controller/AutoListingController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use App\Connector\Marketplace\CiceksepetiConnector;
+use App\Model\DataObject\Marketplace;
+use App\Model\DataObject\VariantProduct;
+use App\Utils\Utility;
+use Doctrine\DBAL\Exception;
+use Pimcore\Db;
+use Random\RandomException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Message\CiceksepetiCategoryUpdateMessage;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Pimcore\Controller\FrontendController;
+use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\DataObject\Data\Link;
+use Pimcore\Model\Asset;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+
+#[IsGranted('ROLE_PIMCORE_ADMIN')]
+class AutoListingController extends FrontendController
+{
+    /**
+     * @Route("/autolisting", name="autolisting_main_page")
+     * @return Response
+     */
+    public function autolistingMainPage(): Response
+    {
+        return $this->render('autolisting/autolisting.html.twig');
+    }
+
+
+
+}
