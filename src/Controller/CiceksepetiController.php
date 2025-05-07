ae70703e397b17dbb7c4d768a8626c17480d7071
enesbt <burdaenesvar@gmail.com>
2025-05-07 11:41:53 +0300

    test page

--- src/Controller/CiceksepetiController.php
+++ src/Controller/CiceksepetiController.php
@@ -60,7 +60,37 @@ class CiceksepetiController extends FrontendController
      */
     public function searchProduct(string $identifier): JsonResponse
     {
-        if (empty($identifier)) {
+        $testData = [
+            'success' => true,
+            'product' => [
+                'id' => '12345',
+                'name' => 'Test Ürün: ' . $identifier,
+                'productCategory' => 'Test Kategori',
+                'variants' => [
+                    [
+                        'id' => '1001',
+                        'iwasku' => 'TST-001',
+                        'variationSize' => 'M',
+                        'variationColor' => 'Kırmızı'
+                    ],
+                    [
+                        'id' => '1002',
+                        'iwasku' => 'TST-002',
+                        'variationSize' => 'L',
+                        'variationColor' => 'Mavi'
+                    ],
+                    [
+                        'id' => '1003',
+                        'iwasku' => 'TST-003',
+                        'variationSize' => 'XL',
+                        'variationColor' => 'Siyah'
+                    ]
+                ]
+            ]
+        ];
+
+        return $this->json($testData);
+        /*if (empty($identifier)) {
             return $this->json(['success' => false, 'message' => 'Ürün kodu belirtilmedi'], 400);
         }
 
@@ -101,7 +131,7 @@ class CiceksepetiController extends FrontendController
         return $this->json([
             'success' => true,
             'product' => $productData
-        ]);
+        ]);*/
     }
 
     /*public function getCiceksepetiListings(): array
