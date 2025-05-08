ad9cfcf6fc45fc33957b3f68dfc02f9a58861697
enesbt <burdaenesvar@gmail.com>
2025-05-08 14:21:22 +0300

    gemini prompt

--- src/MessageHandler/CiceksepetiListingHandler.php
+++ src/MessageHandler/CiceksepetiListingHandler.php
@@ -24,6 +24,7 @@ class CiceksepetiListingHandler
 
     public function __invoke(ProductListingMessage $message)
     {
+        sleep(5);
         $this->listingHelper->saveMessage($message);
         $traceId = $message->getTraceId();
         echo "Ciceksepeti Listing Handler\n";
