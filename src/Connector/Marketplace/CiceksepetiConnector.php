d2e887b33b9ec308838b332f21f73b882654e2e4
enesbt <burdaenesvar@gmail.com>
2025-05-14 10:28:10 +0300

    refactor ciceksepeti autolisting and logging

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -230,7 +230,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
         //24b4c616-2fe0-4059-b01d-9cdc6721eb9c
         //2f641201-53bf-467a-b49d-36b21898aa7a
         //705f24f1-ca36-45f2-85fd-432d6623c9af
-        print_r($this->getBatchRequestResult("2f641201-53bf-467a-b49d-36b21898aa7a"));
+        print_r($this->getBatchRequestResult("705f24f1-ca36-45f2-85fd-432d6623c9af"));
 
         //$this->downloadCategories();
         //$this->getCategoryAttributesAndSaveDatabase(12943);
