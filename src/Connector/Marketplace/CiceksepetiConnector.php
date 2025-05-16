2708a4ffdcc5c163858a197fcde9fd01da7b8dec
enesbt <burdaenesvar@gmail.com>
2025-05-16 16:21:23 +0300

    refactor Refs: #67

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -236,7 +236,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
         //361eb507-5ae3-460b-90e0-f4db0fe5cf19 PROD CM-087
 
 
-        print_r($this->getBatchRequestResult("fce777f8-b7c1-49e8-b6fe-b5baf5f727de"));
+        print_r($this->getBatchRequestResult("223da069-8021-4d3f-beb3-c41a12a86481"));
 
         //$this->downloadCategories();
         //$this->getCategoryAttributesAndSaveDatabase(12943);
