c3d88de33a0f42e1b473c6235d2fe1b7c60a68d1
enesbt <burdaenesvar@gmail.com>
2025-05-16 11:50:45 +0300

    bug fixed

--- src/MessageHandler/CiceksepetiListingHandler.php
+++ src/MessageHandler/CiceksepetiListingHandler.php
@@ -147,7 +147,7 @@ class CiceksepetiListingHandler
                 'deliveryMessageType' => 5,
                 'deliveryType' => 2,
                 'stockQuantity' => 0,
-                'salesPrice' => ($product['salesPrice'] === 0 || $product['salesPrice'] === "0" || !isset($product['salesPrice'])) ? 10000 : $product['salesPrice'] * 2,
+                'salesPrice' => ($product['salesPrice'] === 0 || $product['salesPrice'] === "0" || !isset($product['salesPrice'])) ? 10000 : $product['salesPrice'],
                 'images' => $httpsImages,
                 'Attributes' => $product['Attributes'],
             ];
