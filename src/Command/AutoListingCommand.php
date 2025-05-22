86b4f9e745e582937f13e8d5f3680f3b3d74652e
enesbt <burdaenesvar@gmail.com>
2025-05-22 16:21:57 +0300

    refactor Refs: #68

--- src/Command/AutoListingCommand.php
+++ src/Command/AutoListingCommand.php
@@ -180,9 +180,7 @@ class AutoListingCommand extends AbstractCommand
 
     private function sendToCiceksepeti($productList)
     {
-        $data = [
-            'products' => $productList
-        ];
+        $data['products'] = $productList;
         $json = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
         $ciceksepetiConnector = new CiceksepetiConnector(Marketplace::getById(265384));
         $ciceksepetiConnector->updateProduct($json);
