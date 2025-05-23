ba936d9f90d4958dcb444c2d71149c64344a517a
enesbt <burdaenesvar@gmail.com>
2025-05-23 16:22:10 +0300

    refactor Refs: #68

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -33,7 +33,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
     public function __construct($marketplace)
     {
         parent::__construct($marketplace);
-        $this->httpClient = ScopingHttpClient::forBaseUri($this->httpClient, 'https://sandbox-apis.ciceksepeti.com/api/v1/', [
+        $this->httpClient = ScopingHttpClient::forBaseUri($this->httpClient, 'https://apis.ciceksepeti.com/api/v1/', [
             'headers' => [
                 'x-api-key' => $this->marketplace->getCiceksepetiApiKey(),
                 'Content-Type' => 'application/json'
