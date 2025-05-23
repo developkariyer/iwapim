bee7207ba37dfebb143d41faf6ea31d8704313a2
enesbt <burdaenesvar@gmail.com>
2025-05-23 11:19:44 +0300

    refactor Refs: #68

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -39,7 +39,7 @@ class HelloWorldCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $filePath = PIMCORE_PROJECT_ROOT . '/tmp/wisersell/stores.json';
-        $json = Utility::getCustomCache('stores.json', $filePath);
+        $json = file_get_contents($filePath);
         print_r($json);
 
 
