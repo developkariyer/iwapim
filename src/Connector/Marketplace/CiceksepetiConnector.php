cc2a096d207404e194707569130cbee8c11d5af1
enes <burdaenesvar@gmail.com>
2025-05-25 00:53:21 +0300

    refactor Refs: #68

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -239,7 +239,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
         //d15702ea-5c2b-4e94-9dd2-edd80e99f858
         //87b74947-8d88-456f-9b24-48a6e6be0e31
 
-        print_r($this->getBatchRequestResult("64071f0a-6e07-4209-9308-05c5090e2fc2"));
+        print_r($this->getBatchRequestResult("8dad9bd7-cd4a-43e8-b74a-4dfdaaa823f7"));
 //        $ids = [
 //        ];
 //        foreach ($ids as $id) {
