c4aa8001700d0d115c525b2b83f876c67e6959ce
enesbt <burdaenesvar@gmail.com>
2025-05-26 17:23:51 +0300

    refactor Refs: #68

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -55,7 +55,8 @@ class HelloWorldCommand extends AbstractCommand
                 continue;
             }
             $mainProduct = $mainProducts[0];
-            $this->dimTest($mainProduct->getVariationSize());
+            echo $mainProduct->getProductIdentifier() . "\n";
+            //$this->dimTest($mainProduct->getVariationSize());
 //            $result = $this->findBestAttributeMatch(2000361, $mainProduct->getVariationSize(), true);
 //            echo $mainProduct->getIwasku() . " - " . $mainProduct->getVariationSize() . " -----> ";
 //            print_r($result);
