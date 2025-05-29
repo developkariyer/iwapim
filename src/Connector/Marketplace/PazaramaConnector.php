a87703a1d9e30406ced9e1e1314e084b01b76846
enesbt <burdaenesvar@gmail.com>
2025-05-29 13:10:05 +0300

    pazarama marketplace integration Refs: #69

--- src/Connector/Marketplace/PazaramaConnector.php
+++ src/Connector/Marketplace/PazaramaConnector.php
@@ -60,6 +60,7 @@ class PazaramaConnector extends MarketplaceConnectorAbstract
         $this->prepareToken();
         $response = $this->httpClient->request('GET', static::$apiUrl['offers'], [
             'query' => [
+                'Approved' => false,
                 'page' => 1,
                 'size' => 100
             ]
