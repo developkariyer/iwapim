65f002c0965e84fb954eb5fddc4aa2190acdde84
enesbt <burdaenesvar@gmail.com>
2025-05-29 08:59:25 +0300

    refactor Refs: #68

--- src/MessageHandler/ListingHelperService.php
+++ src/MessageHandler/ListingHelperService.php
@@ -158,11 +158,26 @@ class ListingHelperService
             }
             $imageUrl = $host . $image->getFullPath();
             $imageUrl = preg_replace('/^http:/i', 'https:', $imageUrl);
-            $images[] = [
-                'url' => $imageUrl,
-                'width' => $image->getWidth(),
-                'height' => $image->getHeight()
-            ];
+            $imageUrl = str_replace('mesa.', '', $imageUrl);
+            $parsed = parse_url($imageUrl);
+            $encodedPath = implode('/', array_map('rawurlencode', explode('/', $parsed['path'] ?? '')));
+            $imageUrl = $parsed['scheme'] . '://' . $parsed['host'] . $encodedPath;
+            if (!empty($parsed['query'])) {
+                $imageUrl .= '?' . $parsed['query'];
+            }
+            $headers = @get_headers($imageUrl);
+            if ($headers && strpos($headers[0], '200') !== false) {
+                $images[] = [
+                    'url' => $imageUrl,
+                    'width' => $image->getWidth(),
+                    'height' => $image->getHeight()
+                ];
+            }
+//            $images[] = [
+//                'url' => $imageUrl,
+//                'width' => $image->getWidth(),
+//                'height' => $image->getHeight()
+//            ];
 //            $headers = @get_headers($imageUrl);
 //            if ($headers && strpos($headers[0], '200') !== false) {
 //                $images[] = [
