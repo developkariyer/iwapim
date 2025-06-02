ba7390cb3f1fd24d22dcfd603418de5609077394
enesbt <burdaenesvar@gmail.com>
2025-06-02 16:52:37 +0300

    refactor Refs: #68

--- src/MessageHandler/CiceksepetiListingHandler.php
+++ src/MessageHandler/CiceksepetiListingHandler.php
@@ -63,11 +63,11 @@ class CiceksepetiListingHandler
         $this->logger->info("[" . __METHOD__ . "] ✅ Filled Attribute Data ");
         $normalizedCiceksepetiData = $this->normalizeCiceksepetiData($filledAttributeData);
         $this->logger->info("[" . __METHOD__ . "] ✅ Normalized Ciceksepeti Data ");
-        print_r($normalizedCiceksepetiData);
-//        $ciceksepetiConnector = new CiceksepetiConnector(Marketplace::getById(265384));
-//        $result = $ciceksepetiConnector->createListing($normalizedCiceksepetiData);
-//        $this->logger->info("✅ [CiceksepetiConnector] Result batch:\n" . json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-//        print_r($result);
+//        print_r($normalizedCiceksepetiData);
+        $ciceksepetiConnector = new CiceksepetiConnector(Marketplace::getById(265384));
+        $result = $ciceksepetiConnector->createListing($normalizedCiceksepetiData);
+        $this->logger->info("✅ [CiceksepetiConnector] Result batch:\n" . json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        print_r($result);
     }
 
     private function controlGeminiFilledData($data)
