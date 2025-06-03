1330551ff1430a2e55bf529efdebeab2fb9c39e2
enesbt <burdaenesvar@gmail.com>
2025-06-03 10:48:36 +0300

    refactor Refs: #68

--- src/Connector/Marketplace/CiceksepetiConnector.php
+++ src/Connector/Marketplace/CiceksepetiConnector.php
@@ -560,6 +560,7 @@ class CiceksepetiConnector extends MarketplaceConnectorAbstract
     public function updateProduct($data)
     {
         $response = $this->httpClient->request('PUT', static::$apiUrl['offers'], ['body' => $data]);
+        print_r($response->toArray());
         $statusCode = $response->getStatusCode();
         if ($statusCode !== 200) {
             echo "Error: $statusCode\n";
