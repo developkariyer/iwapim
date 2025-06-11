71bb07d1beaa16843c3d5212d9494c36cd09a7a4
enesbt <burdaenesvar@gmail.com>
2025-06-11 11:56:48 +0300

    refactor Refs: #57

--- src/MessageHandler/ListingHelperService.php
+++ src/MessageHandler/ListingHelperService.php
@@ -99,7 +99,8 @@ class ListingHelperService
         $description = $parentApiJsonShopify['descriptionHtml'] ?? '';
         $stockQuantity = $apiJsonShopify['inventoryQuantity'] ?? '';
         $salesPrice = $apiJsonShopify['price'] ?? '';
-        $images = $this->getShopifyImages($parentApiJsonShopify);
+        //$images = $this->getShopifyImages($parentApiJsonShopify);
+        $images = [];
         if (!$shopifyIsActive || empty($images) || empty($title) || empty($description) || empty($stockQuantity) || empty($salesPrice) ) {
             return [];
         }
