9e83d648efe6f824a98a94c262815c913705dfab
enesbt <burdaenesvar@gmail.com>
2025-06-11 10:28:39 +0300

    refactor Refs: #57

--- src/Command/AutoListingCommand.php
+++ src/Command/AutoListingCommand.php
@@ -149,28 +149,27 @@ class AutoListingCommand extends AbstractCommand
 
     private function processNewList($groupedByMainCode, $targetMarketplaceId, $referenceMarketplaceId, $toMarketplace): void
     {
-//        foreach ($groupedByMainCode as $mainCode => $variantIds) {
-//            $message = new ProductListingMessage(
-//                'list',
-//                $targetMarketplaceId,
-//                $referenceMarketplaceId,
-//                'admin',
-//                $variantIds,
-//                [],
-//                1,
-//                'test',
-//                $this->logger
-//            );
-//            $transportName = $this->resolveTransportName($toMarketplace);
-//            $stamps = [new TransportNamesStamp([$transportName])];
-//            $this->bus->dispatch($message, $stamps);
-//            $this->logger->info("[" . __METHOD__ . "] ✅ Created Message for Main Product Code: $mainCode");
-//        }
+        foreach ($groupedByMainCode as $mainCode => $variantIds) {
+            $message = new ProductListingMessage(
+                'list',
+                $targetMarketplaceId,
+                $referenceMarketplaceId,
+                'admin',
+                $variantIds,
+                [],
+                1,
+                'test',
+                $this->logger
+            );
+            $transportName = $this->resolveTransportName($toMarketplace);
+            $stamps = [new TransportNamesStamp([$transportName])];
+            $this->bus->dispatch($message, $stamps);
+            $this->logger->info("[" . __METHOD__ . "] ✅ Created Message for Main Product Code: $mainCode");
+        }
     }
 
     private function processUpdateList($updateProductList, $targetMarketplaceId, $referenceMarketplaceId, $toMarketplace): void
     {
-        print_r($updateProductList);
 //        $message = new ProductListingMessage(
 //            'update_list',
 //            $targetMarketplaceId,
