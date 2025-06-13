5d5d9be9449ab26be9d689d1e25e31e0fca9074f
enesbt <burdaenesvar@gmail.com>
2025-06-13 15:32:28 +0300

    refactor Refs: #57

--- src/Connector/Marketplace/HepsiburadaConnector.php
+++ src/Connector/Marketplace/HepsiburadaConnector.php
@@ -393,6 +393,8 @@ class HepsiburadaConnector extends MarketplaceConnectorAbstract
             'headers' => array_merge($formData->getPreparedHeaders()->toArray(), [
                 'Authorization' => 'Basic ' . base64_encode($this->marketplace->getSellerId() . ':' . $this->marketplace->getServiceKey()),
                 'User-Agent' => 'colorfullworlds_dev',
+                'Accept' => 'application/json',
+                'Content-Type' => 'multipart/form-data'
             ]),
             'body' => $formData->bodyToIterable()
         ]);
