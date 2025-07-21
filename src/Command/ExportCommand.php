30691a729ad9efa9779242c3813d22d05687c8de
enesbt <burdaenesvar@gmail.com>
2025-07-21 13:25:19 +0300

    export data

--- src/Command/ExportCommand.php
+++ src/Command/ExportCommand.php
@@ -19,21 +19,36 @@ class ExportCommand extends AbstractCommand
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $productListing = new ProductListing();
-        $productListing->setLimit(10);
-        $productListing->setOffset(0);
-        $products = $productListing->load();
-        foreach ($products as $product) {
-            $level = $product->getProductLevel();
-            $productKey = $product->getKey();
-            echo  $productKey."\n";
-            echo  $level."\n";
+//        $productListing = new ProductListing();
+//        $productListing->setLimit(10);
+//        $productListing->setOffset(0);
+//        $products = $productListing->load();
+//        foreach ($products as $product) {
+//            $level = $product->getProductLevel();
+//            $productKey = $product->getKey();
+//            echo  $productKey."\n";
+//            echo  $level."\n";
+//
+//
+//        }
 
-
+        $mainProducts = $this->getMainProducts(10, 0);
+        foreach ($mainProducts as $product) {
+            $name = $product->getName();
+            echo $name."\n";
         }
 
-
         return Command::SUCCESS;
+    }
+
+    private function getMainProducts($limit, $offset)
+    {
+        $mainProductListing = new ProductListing();
+        $mainProductListing->setLimit($limit);
+        $mainProductListing->setOffset($offset);
+        $mainProductListing->setCondition("productLevel = 0");
+        $mainProducts = $mainProductListing->load();
+        return $mainProducts;
     }
 
 }
