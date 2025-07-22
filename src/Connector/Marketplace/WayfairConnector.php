1184666b5d4cc00e7253869272b585b963f48bfa
enesbt <burdaenesvar@gmail.com>
2025-07-22 10:45:15 +0300

    wayfair new marketplace integration

--- src/Connector/Marketplace/WayfairConnector.php
+++ src/Connector/Marketplace/WayfairConnector.php
@@ -235,7 +235,7 @@ class WayfairConnector extends MarketplaceConnectorAbstract
             query getDropshipPurchaseOrders {
                 getDropshipPurchaseOrders(
                     limit: 10,
-                    hasResponse: false,
+                    hasResponse: true,
                     sortOrder: ASC
                 ) {
                     poNumber,
