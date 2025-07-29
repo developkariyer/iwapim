9925dde05fefc2bfeefe0c48e2db2b80b16b02c1
enesbt <burdaenesvar@gmail.com>
2025-07-29 10:53:15 +0300

    wayfair new marketplace

--- src/Connector/Marketplace/WayfairConnector.php
+++ src/Connector/Marketplace/WayfairConnector.php
@@ -124,7 +124,7 @@ class WayfairConnector extends MarketplaceConnectorAbstract
         //$this->queryOpenOrdersSandbox();
         //$this->getDropshipOrdersSandbox();
         //$this->acceptDropshipOrdersSandbox('TEST_11620177', 'ASH_P384_MOUNTVALLEYDINING');
-        $this->sendShipmentSandbox(275550, 'TEST_14384288', 'ASH_P008-827_ROCKINGCHAIR');
+        $this->sendShipmentSandbox(275550, 'TEST_71288393', 'ASH_P008-898_ADIRONDACKCHAIR');
 
 //        try {
 //            $sqlLastUpdatedAt = "
