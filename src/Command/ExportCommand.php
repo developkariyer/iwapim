7f710eb94c471aa586acf0edfcbc2e5c3c537df9
enesbt <burdaenesvar@gmail.com>
2025-07-30 16:28:51 +0300

    export product refactor

--- src/Command/ExportCommand.php
+++ src/Command/ExportCommand.php
@@ -378,7 +378,7 @@ class ExportCommand extends AbstractCommand
                 $parsedDimensionValues[] = [$key, $part];
                 continue;
             }
-            if (preg_match('/(\d+(?:[.,]\d+)?)[xX](\d+(?:[.,]\d+)?)[xX](\d+(?:[.,]\d+)?)(?:\s*(mm|cm|m))?/i', $part, $matches)) {
+            if (preg_match('/^(\d+(?:[.,]\d+)?)[xX](\d+(?:[.,]\d+)?)[xX](\d+(?:[.,]\d+)?)(?:\s*(?:mm|cm|m))?$/i', $part, $matches)) {
                 $a = (float) str_replace(',', '.', $matches[1]);
                 $b = (float) str_replace(',', '.', $matches[2]);
                 $c = (float) str_replace(',', '.', $matches[3]);
