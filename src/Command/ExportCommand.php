d8c93ed56a74f6eb6dceae559ca970d791a72e27
enesbt <burdaenesvar@gmail.com>
2025-07-31 09:57:36 +0300

    export product refactor

--- src/Command/ExportCommand.php
+++ src/Command/ExportCommand.php
@@ -123,10 +123,10 @@ class ExportCommand extends AbstractCommand
             echo "offset = $offset\n";
             $offset += $limit;
         }
-        //$allProducts = $this->setVariantCustomData($allProducts);
-//        $filePath = PIMCORE_PROJECT_ROOT . '/tmp/exportProduct.json';
-//        file_put_contents($filePath, json_encode($allProducts, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-//        echo "Exported to: " . $filePath . "\n";
+        $allProducts = $this->setVariantCustomData($allProducts);
+        $filePath = PIMCORE_PROJECT_ROOT . '/tmp/exportProduct.json';
+        file_put_contents($filePath, json_encode($allProducts, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        echo "Exported to: " . $filePath . "\n";
 
         $txtFilePath = PIMCORE_PROJECT_ROOT . '/tmp/size_mappings.txt';
         file_put_contents($txtFilePath, $txtOutput);
